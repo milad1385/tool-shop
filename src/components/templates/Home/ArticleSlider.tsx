4d24e958d8288429137f401bbf7ad1a21bf7c41e
Milad salamian <miladsalami1385@gmail.com>
2025-03-15 08:54:37 -0700

    responsive Article Slider

--- src/components/templates/Home/ArticleSlider.tsx
+++ src/components/templates/Home/ArticleSlider.tsx
@@ -1,6 +1,7 @@
 "use client";
 import ArticleBox from "@/components/modules/ArticleBox";
 import React from "react";
+import { Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 function ArticleSlider() {
@@ -11,7 +12,21 @@ function ArticleSlider() {
     { id: 4, title: "راهنمای خرید انواع مته", link: "", image: "blog-1.jpg" },
   ];
   return (
-    <Swiper slidesPerView={3} spaceBetween={15}>
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={15}
+      autoplay={{
+        delay: 4000,
+        disableOnInteraction: false,
+      }}
+      modules={[Autoplay]}
+      breakpoints={{
+        0: { slidesPerView: 1 },
+        500: { slidesPerView: 2 },
+        900: { slidesPerView: 2 },
+        992: { slidesPerView: 3 },
+      }}
+    >
       {articles.map((article) => (
         <SwiperSlide key={article.id}>
           <ArticleBox {...article} />
