e13cd934f4d379a3350cb2def4159c74f8ded6e5
Milad salamian <miladsalami1385@gmail.com>
2025-03-15 08:49:56 -0700

    responsive Suggested Product Slider

--- src/components/templates/Home/SuggestedProductSlider.tsx
+++ src/components/templates/Home/SuggestedProductSlider.tsx
@@ -3,6 +3,7 @@ import ProductBox from "@/components/modules/ProductBox";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import { Autoplay } from "swiper/modules";
 
 function SuggestedProductSlider() {
   const products = [
@@ -48,7 +49,22 @@ function SuggestedProductSlider() {
     },
   ];
   return (
-    <Swiper slidesPerView={4} spaceBetween={15}>
+    <Swiper
+      slidesPerView={4}
+      spaceBetween={15}
+      autoplay={{
+        delay: 2200,
+        disableOnInteraction: false,
+      }}
+      modules={[Autoplay]}
+      breakpoints={{
+        300: { slidesPerView: 1 },
+        500: { slidesPerView: 2 },
+        768: { slidesPerView: 3 },
+        992: { slidesPerView: 3 },
+        1024: { slidesPerView: 4 },
+      }}
+    >
       {products.map((product) => (
         <SwiperSlide key={product.id}>
           <ProductBox {...product} />
