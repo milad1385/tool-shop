da8b2ad6547d757f9aa074c5bccff8566517f7ae
Milad salamian <miladsalami1385@gmail.com>
2025-04-09 12:59:16 -0700

    fix question page structure

--- src/app/(main)/questions/page.tsx
+++ src/app/(main)/questions/page.tsx
@@ -3,20 +3,16 @@ import Image from "next/image";
 
 function page() {
   return (
-    <>
-      <section>
-        <div className="relative flex justify-center items-center select-none">
-          <Image
-            src="/images/bg-question.jpg"
-            alt="question-image"
-            width={1920}
-            height={1080}
-            className="object-cover lg:h-auto h-60"
-          />
-          <QuetionSearchInput />
-        </div>
-      </section>
-    </>
+    <section className="relative flex justify-center items-center select-none">
+      <Image
+        src="/images/bg-question.jpg"
+        alt="question-image"
+        width={1920}
+        height={1080}
+        className="object-cover lg:h-auto h-60"
+      />
+      <QuetionSearchInput />
+    </section>
   );
 }
 
