07331a3182c3bca1b19fef4f40b449d7a1a75fda
Milad salamian <miladsalami1385@gmail.com>
2025-04-10 05:11:42 -0700

    add articles page

--- src/app/(main)/articles/page.tsx
+++ src/app/(main)/articles/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Breadcrumb from "@/components/modules/Breadcrumb";
+import Articles from "@/components/templates/articles/Articles";
+import React, { Suspense } from "react";
 
 function page() {
   return (
-    <div>page</div>
-  )
+    <div className="container mt-24 md:mt-48">
+      <Breadcrumb
+        links={[
+          { id: 1, href: "/", name: "خانه" },
+          { id: 2, href: "/articles", name: "مقاله ها" },
+        ]}
+      />
+      <Articles />
+    </div>
+  );
 }
 
-export default page
+export default page;
---
+++ src/components/templates/articles/Articles.tsx
@@ -0,0 +1,22 @@
+import ArticleBox from "@/components/modules/ArticleBox";
+import React from "react";
+
+function Articles() {
+  const articles = [
+    { id: 1, title: "راهنمای خرید انواع مته", link: "", image: "blog-1.jpg" },
+    { id: 2, title: "تعمیر قلاب ماهیگیری", link: "", image: "blog-2.jpg" },
+    { id: 3, title: "تمیز کردن لوازم کارگاهی", link: "", image: "blog-3.jpg" },
+    { id: 4, title: "راهنمای خرید انواع مته", link: "", image: "blog-1.jpg" },
+    { id: 5, title: "تعمیر قلاب ماهیگیری", link: "", image: "blog-2.jpg" },
+    { id: 6, title: "راهنمای خرید انواع مته", link: "", image: "blog-1.jpg" },
+  ];
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {articles.map((article) => (
+        <ArticleBox {...article} key={article.id} />
+      ))}
+    </div>
+  );
+}
+
+export default Articles;
