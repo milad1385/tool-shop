d998fb344d84b284b847f3e7d38db8a183e1c4fa
Milad salamian <miladsalami1385@gmail.com>
2025-04-11 13:19:22 -0700

    add blog detail title

--- src/app/(main)/blog/[id]/page.tsx
+++ src/app/(main)/blog/[id]/page.tsx
@@ -1,5 +1,8 @@
 import Breadcrumb from "@/components/modules/Breadcrumb";
 import React from "react";
+import { FaClock } from "react-icons/fa";
+import { IoFolderOpenOutline } from "react-icons/io5";
+import { LuClock3 } from "react-icons/lu";
 
 function page() {
   return (
@@ -11,6 +14,24 @@ function page() {
           { id: 3, href: "/blog/1", name: "مقاله راجع فلان" },
         ]}
       />
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-9 bg-white p-4 rounded-3xl leading-8">
+          <div className="border-r-4 bg-slate-100 border-yellow-400 mb-4 rounded-2xl p-4">
+            <h1 className="font-Lalezar text-xl text-zinc-700">لورم ایپسوم متن ساختگی با تولید سادگی</h1>
+            <div className="flex items-center gap-4 mt-1">
+              <div className="flex items-center gap-x-1">
+                <LuClock3 className="text-lg text-zinc-700" />
+                <span className="text-[13px]">14 تیر 1402</span>
+              </div>
+              <div className="flex items-center gap-x-1">
+                <IoFolderOpenOutline className="text-lg text-zinc-700" />
+                <span className="text-[13px]"> ابزارآلات</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-3 bg-white rounded-2xl"></div>
+      </div>
     </div>
   );
 }
