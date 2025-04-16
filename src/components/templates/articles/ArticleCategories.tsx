00fb3655b20b9be9f587582d464b14584452979e
Milad salamian <miladsalami1385@gmail.com>
2025-04-16 06:41:29 -0700

    generate article category dynamicly

--- src/components/templates/articles/ArticleCategories.tsx
+++ src/components/templates/articles/ArticleCategories.tsx
@@ -3,28 +3,41 @@ import ArticleCategory from "./ArticleCategory";
 import ArticleSectionTitle from "./ArticleSectionTitle";
 
 function ArticleCategories() {
+  const articleCategories = [
+    {
+      id : 1,
+      title: "دسته بندی 1",
+      subCategories: [
+        { id: 1, title: "زیر دسته بندی 1" },
+        { id: 2, title: "زیر دسته بندی 2" },
+        { id: 3, title: "زیر دسته بندی 3" },
+      ],
+    },
+    {
+      id : 2,
+      title: "دسته بندی 2",
+      subCategories: [
+        { id: 1, title: "زیر دسته بندی 1" },
+        { id: 2, title: "زیر دسته بندی 2" },
+        { id: 3, title: "زیر دسته بندی 3" },
+      ],
+    },
+    {
+      id : 3,
+      title: "دسته بندی 3",
+      subCategories: [],
+    },
+  ];
   return (
     <div className="bg-white p-4 rounded-3xl mb-4 leading-8">
       <ArticleSectionTitle title="دسته بندی ها" />
       <ul className="space-y-3">
-        <ArticleCategory
-          title="دسته بندی 1"
-          subCategories={[
-            { id: 1, title: "زیر دسته بندی 1" },
-            { id: 2, title: "زیر دسته بندی 2" },
-            { id: 3, title: "زیر دسته بندی 3" },
-          ]}
-        />
-
-        <ArticleCategory
-          title="دسته بندی 2"
-          subCategories={[
-            { id: 1, title: "زیر دسته بندی 1" },
-            { id: 2, title: "زیر دسته بندی 2" },
-            { id: 3, title: "زیر دسته بندی 3" },
-          ]}
-        />
-        <ArticleCategory title="دسته بندی 3" subCategories={[]} />
+        {articleCategories.map((articleCategory) => (
+          <ArticleCategory
+            {...articleCategory}
+            key={articleCategory.id}
+          />
+        ))}
       </ul>
     </div>
   );
