c86fe434e8e846582c0bc978fb424790f415012c
Milad salamian <miladsalami1385@gmail.com>
2025-05-15 08:12:45 -0700

    generate footer menu dynamicly

--- src/components/modules/main/FooterMenu.tsx
+++ src/components/modules/main/FooterMenu.tsx
@@ -1,9 +1,7 @@
+import { footerMenus } from "@/constants/data";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import { BsBasket2Fill } from "react-icons/bs";
-import { FaHeart, FaTools } from "react-icons/fa";
-import { IoIosHome } from "react-icons/io";
 
 function FooterMenu() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -22,76 +20,48 @@ function FooterMenu() {
       <div className="fixed bottom-0 z-20 h-[72.5px] w-full bg-[url('/images/mobile-footer-bg.svg')] bg-cover bg-center bg-no-repeat pb-2 pt-3 lg:hidden shadow-2xl">
         <nav className="container">
           <ul className="flex w-full items-center justify-between gap-2">
-            <li className="w-[56px]">
-              <Link
-                className={`flex flex-col items-center text-[12px] gap-1 relative transition-all ${
-                  pathname === "/" ? activeStyle : "text-gray-700/60"
-                }`}
-                href="/"
-              >
-                <IoIosHome className="text-xl" />
-                <p className="font-bold">خانه</p>
-              </Link>
-            </li>
-            <li className="w-[56px]">
-              <Link
-                className={`flex flex-col items-center text-[12px] gap-1 relative transition-all ${
-                  pathname.includes("/products")
-                    ? activeStyle
-                    : "text-gray-700/60"
-                }`}
-                href="/products"
-              >
-                <FaTools className="text-xl" />
-                <p className="font-bold">ابزار ها</p>
-              </Link>
-            </li>
-            <li className="w-[56px]">
-              <Link
-                className={`flex-col text-[12px] gap-1  transition-all relative -top-9 bg-zinc-800 rounded-full size-14 flex justify-center items-center after:hidden`}
-                href="/cart"
-              >
-                <svg
-                  stroke="currentColor"
-                  fill="currentColor"
-                  strokeWidth="0"
-                  viewBox="0 0 576 512"
-                  color="#fff"
-                  height="22"
-                  width="22"
-                  xmlns="http://www.w3.org/2000/svg"
-                  style={{ color: "rgb(255, 255, 255)" }}
-                >
-                  <path d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z"></path>
-                </svg>
-              </Link>
-            </li>
-            <li className="w-[56px]">
-              <Link
-                className={`flex flex-col items-center text-[12px] gap-1  relative transition-all ${
-                  pathname.includes("/orders")
-                    ? activeStyle
-                    : "text-gray-700/60"
-                }`}
-                href="/p-user/orders"
-              >
-                <BsBasket2Fill className="text-xl" />
-                <p className="font-bold">سفارشات</p>
-              </Link>
-            </li>
-            <li className="w-[56px]">
-              <Link
-                className={`flex flex-col items-center text-[12px] gap-1  relative transition-all ${
-                  pathname.includes("/favorites")
-                    ? activeStyle
-                    : "text-gray-700/60"
-                }`}
-                href="/p-user/favorites"
-              >
-                <FaHeart className="text-xl" />
-                <p className="font-bold">علاقه مندی</p>
-              </Link>
-            </li>
+            {footerMenus.map((footer) => {
+              if (footer.title) {
+                return (
+                  <li className="w-[56px]" key={footer.id}>
+                    <Link
+                      className={`flex flex-col items-center text-[12px] gap-1 relative transition-all ${
+                        pathname === footer.href
+                          ? activeStyle
+                          : "text-gray-700/60"
+                      }`}
+                      href={footer.href}
+                    >
+                      {footer.icon}
+                      <p className="font-bold">{footer.title}</p>
+                    </Link>
+                  </li>
+                );
+              } else {
+                return (
+                  <li className="w-[56px]" key={footer.id}>
+                    <Link
+                      className={`flex-col text-[12px] gap-1  transition-all relative -top-9 bg-zinc-800 rounded-full size-14 flex justify-center items-center after:hidden`}
+                      href={footer.href}
+                    >
+                      <svg
+                        stroke="currentColor"
+                        fill="currentColor"
+                        strokeWidth="0"
+                        viewBox="0 0 576 512"
+                        color="#fff"
+                        height="22"
+                        width="22"
+                        xmlns="http://www.w3.org/2000/svg"
+                        style={{ color: "rgb(255, 255, 255)" }}
+                      >
+                        <path d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z"></path>
+                      </svg>
+                    </Link>
+                  </li>
+                );
+              }
+            })}
           </ul>
         </nav>
       </div>
--- src/constants/data.tsx
+++ src/constants/data.tsx
@@ -5,9 +5,12 @@ import {
   HiOutlineFaceSmile,
   HiOutlineSquares2X2,
   HiOutlineUser,
-} from "react-icons/hi2";;
+} from "react-icons/hi2";
 import { LiaComments } from "react-icons/lia";
 import { IoTicketOutline } from "react-icons/io5";
+import { IoIosHome } from "react-icons/io";
+import { FaHeart, FaTools } from "react-icons/fa";
+import { BsBasket2Fill } from "react-icons/bs";
 
 export const priority = [
   { id: 1, value: 1, label: "بسیار بالا" },
@@ -22,47 +25,93 @@ export const departments = [
 ];
 
 export const links = [
-    {
-      id: 1,
-      title: "پیشخوان",
-      icon: <HiOutlineSquares2X2 className="text-2xl" />,
-      href: "/p-user",
-    },
-    {
-      id: 2,
-      title: "سفارش ها",
-      icon: <HiOutlineCreditCard className="text-2xl" />,
-      href: "/p-user/orders",
-    },
-    {
-      id: 3,
-      title: "لیست علاقه مندی",
-      icon: <HiOutlineFaceSmile className="text-2xl" />,
-      href: "/p-user/favorites",
-    },
-    {
-      id: 4,
-      title: "نظرات",
-      icon: <LiaComments className="text-2xl" />,
-      href: "/p-user/comments",
-    },
-    {
-      id: 44,
-      title: "تیکت ها",
-      icon: <IoTicketOutline className="text-2xl" />,
-      href: "/p-user/tickets",
-    },
-    {
-      id: 5,
-      title: "دانلود ها",
-      icon: <HiOutlineDocumentDownload className="text-2xl" />,
-      href: "/p-user/downloads",
-    },
-    {
-      id: 6,
-      title: "جزییات حساب کاربری",
-      icon: <HiOutlineUser className="text-2xl" />,
-      href: "/p-user/information",
-    },
-   
-  ];
+  {
+    id: 1,
+    title: "پیشخوان",
+    icon: <HiOutlineSquares2X2 className="text-2xl" />,
+    href: "/p-user",
+  },
+  {
+    id: 2,
+    title: "سفارش ها",
+    icon: <HiOutlineCreditCard className="text-2xl" />,
+    href: "/p-user/orders",
+  },
+  {
+    id: 3,
+    title: "لیست علاقه مندی",
+    icon: <HiOutlineFaceSmile className="text-2xl" />,
+    href: "/p-user/favorites",
+  },
+  {
+    id: 4,
+    title: "نظرات",
+    icon: <LiaComments className="text-2xl" />,
+    href: "/p-user/comments",
+  },
+  {
+    id: 44,
+    title: "تیکت ها",
+    icon: <IoTicketOutline className="text-2xl" />,
+    href: "/p-user/tickets",
+  },
+  {
+    id: 5,
+    title: "دانلود ها",
+    icon: <HiOutlineDocumentDownload className="text-2xl" />,
+    href: "/p-user/downloads",
+  },
+  {
+    id: 6,
+    title: "جزییات حساب کاربری",
+    icon: <HiOutlineUser className="text-2xl" />,
+    href: "/p-user/information",
+  },
+];
+
+export const footerMenus = [
+  {
+    id: 1,
+    title: "خانه",
+    icon: <IoIosHome className="text-xl" />,
+    href: "/",
+  },
+  {
+    id: 2,
+    title: "ابزار ها",
+    icon: <FaTools className="text-xl" />,
+    href: "/products",
+  },
+  {
+    id: 3,
+    title: "",
+    icon: (
+      <svg
+        stroke="currentColor"
+        fill="currentColor"
+        strokeWidth="0"
+        viewBox="0 0 576 512"
+        color="#fff"
+        height="22"
+        width="22"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{ color: "rgb(255, 255, 255)" }}
+      >
+        <path d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z"></path>
+      </svg>
+    ),
+    href: "/cart",
+  },
+  {
+    id: 4,
+    title: "سفارشات",
+    icon: <BsBasket2Fill className="text-xl" />,
+    href: "/p-user/orders",
+  },
+  {
+    id: 5,
+    title: "علاقه مندی",
+    icon: <FaHeart className="text-xl" />,
+    href: "/p-user/favorites",
+  },
+];
