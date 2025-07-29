76fce0e5da4649021b64f98901bc9645dff9ee98
Milad salamian <miladsalami1385@gmail.com>
2025-07-29 13:43:15 -0700

    add shadow elem in categories component

--- src/components/templates/Home/Categories.tsx
+++ src/components/templates/Home/Categories.tsx
@@ -4,9 +4,11 @@ import Title from "@/components/modules/main/Title";
 
 function Categories() {
   return (
-    <section className="py-10">
+    <section className="py-10 relative">
       <Title title="دسته بندی محصولات" />
       <CategoriesSlider />
+      <div className="hidden lg:block absolute  top-40 left-0 -z-50 w-[0.01px] h-[0.01px] bg-[#ece169] rounded-full shadow-[0px_0px_190px_150px_#ece169]"></div>
+      <div className="hidden lg:block absolute  top-96 right-0 -z-50 w-[0.01px] h-[0.01px] bg-[#ece169] rounded-full shadow-[0px_0px_190px_150px_#ece169]"></div>
     </section>
   );
 }
