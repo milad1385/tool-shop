a4055160fc551ecc7fbf266be3a7880b51f2abc1
Milad salamian <miladsalami1385@gmail.com>
2025-07-30 06:47:59 -0700

    add create discount form

--- src/app/p-admin/discount/page.tsx
+++ src/app/p-admin/discount/page.tsx
@@ -1,11 +1,13 @@
 import Container from '@/components/modules/p-admin/Container'
 import PageTitle from '@/components/modules/p-admin/PageTitle'
+import CreateDiscount from '@/components/templates/p-admin/CreateDiscount'
 import React from 'react'
 
 function page() {
   return (
     <Container>
       <PageTitle content="لیست کد تخفیف" />
+      <CreateDiscount/>
     </Container>
   )
 }
---
+++ src/components/templates/p-admin/CreateDiscount.tsx
@@ -0,0 +1,87 @@
+"use client";
+import Button from "@/components/ui/Button";
+import Input from "@/components/ui/Input";
+import { createDiscountSchema } from "@/validators/frontend/discount.validator";
+import { yupResolver } from "@hookform/resolvers/yup";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function CreateDiscount() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    setValue,
+    reset,
+  } = useForm({
+    resolver: yupResolver(createDiscountSchema),
+  });
+  return (
+    <form className="section-box">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-7">
+        <Input
+          register={register}
+          errors={errors}
+          name="title"
+          type="text"
+          label="عنوان کد"
+          disable={false}
+          className="bg-gray-50"
+          labelClassName="!text-lg font-Iran"
+        />
+        <Input
+          register={register}
+          errors={errors}
+          name="percentage"
+          type="text"
+          label="درصد"
+          className="bg-gray-50"
+          disable={false}
+          labelClassName="!text-lg font-Iran"
+        />
+
+        <Input
+          register={register}
+          errors={errors}
+          name="usage"
+          type="text"
+          className="bg-gray-50"
+          label="تعداد مصرف"
+          disable={false}
+          labelClassName="!text-lg font-Iran"
+        />
+
+        <Input
+          register={register}
+          errors={errors}
+          name="product"
+          type="select"
+          options={[
+            { id: 1, label: "دریل", value: "deral" },
+            { id: 2, label: "سنگ بر", value: "stone" },
+          ]}
+          label="محصول"
+          disable={false}
+          labelClassName="!text-lg font-Iran"
+          className="!bg-gray-50"
+        />
+      </div>
+      <div className="flex items-center gap-x-4">
+        <Button type="submit" className="!w-[200px] mt-10">
+          ایجاد کد تخفیف 
+        </Button>
+        <Button
+          onClick={() => {
+            reset();
+          }}
+          type="reset"
+          className="!w-[200px] mt-10 !bg-red-500"
+        >
+          لغو
+        </Button>
+      </div>
+    </form>
+  );
+}
+
+export default CreateDiscount;
