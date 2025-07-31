9e992d9c56b48522d9ee72240ca3df0268d34c28
Milad salamian <miladsalami1385@gmail.com>
2025-07-31 12:09:15 -0700

    add seller request page in admin panel

---
+++ src/app/p-admin/sellers-request/page.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/modules/p-admin/Container";
+import PageTitle from "@/components/modules/p-admin/PageTitle";
+import React from "react";
+
+function page() {
+  return (
+    <Container>
+      <PageTitle content="لیست درخواست فروشندگان" />
+    </Container>
+  );
+}
+
+export default page;
