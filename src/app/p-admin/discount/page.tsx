2a7b104a7ddfd4ed015e73d9cf4bdf5d4d1799e3
Milad salamian <miladsalami1385@gmail.com>
2025-07-31 11:44:41 -0700

    add table operaton component in discount page in admin panel

--- src/app/p-admin/discount/page.tsx
+++ src/app/p-admin/discount/page.tsx
@@ -1,24 +1,16 @@
 import Container from "@/components/modules/p-admin/Container";
-import Filters from "@/components/modules/p-admin/Filters";
 import PageTitle from "@/components/modules/p-admin/PageTitle";
-import Search from "@/components/modules/p-admin/Search";
+import TableOpration from "@/components/modules/p-admin/TableOpration";
 import CreateDiscount from "@/components/templates/p-admin/CreateDiscount";
 import DiscountList from "@/components/templates/p-admin/DiscountList";
 import { DiscountFilterOptions } from "@/constants/data";
-import React from "react";
 
 function page() {
   return (
     <Container>
       <PageTitle content="ایجاد کد تخفیف" />
       <CreateDiscount />
-      <div className="flex items-center justify-between my-8">
-        <PageTitle content="لیست کد تخفیف" />
-        <div className="flex items-center gap-x-4">
-          <Filters filterField="status" options={DiscountFilterOptions} />
-          <Search/>
-        </div>
-      </div>
+      <TableOpration pageTitle="لیست کد های تخفیف" options={DiscountFilterOptions}/>
       <DiscountList />
     </Container>
   );
