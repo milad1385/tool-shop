26103ec1be5a7461d0ed4c0d1c9442aa5c14b454
Milad salamian <miladsalami1385@gmail.com>
2025-07-31 09:41:02 -0700

    add search component in discount page

--- src/app/p-admin/discount/page.tsx
+++ src/app/p-admin/discount/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/components/modules/p-admin/Container";
 import Filters from "@/components/modules/p-admin/Filters";
 import PageTitle from "@/components/modules/p-admin/PageTitle";
+import Search from "@/components/modules/p-admin/Search";
 import CreateDiscount from "@/components/templates/p-admin/CreateDiscount";
 import DiscountList from "@/components/templates/p-admin/DiscountList";
 import { DiscountFilterOptions } from "@/constants/data";
@@ -13,10 +14,10 @@ function page() {
       <CreateDiscount />
       <div className="flex items-center justify-between my-8">
         <PageTitle content="لیست کد تخفیف" />
-        <Filters
-          filterField="status"
-          options={DiscountFilterOptions}
-        />
+        <div className="flex items-center gap-x-4">
+          <Filters filterField="status" options={DiscountFilterOptions} />
+          <Search/>
+        </div>
       </div>
       <DiscountList />
     </Container>
