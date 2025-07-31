f52377de1f853bddf403190656a2a1ab03e1d133
Milad salamian <miladsalami1385@gmail.com>
2025-07-31 12:08:10 -0700

    fix admin panel side bar component styles

--- src/components/modules/p-admin/Sidebar.tsx
+++ src/components/modules/p-admin/Sidebar.tsx
@@ -7,7 +7,7 @@ function Sidebar() {
     <div className="col-span-2 bg-white">
       <div className="sticky top-0 p-5">
         <h1 className="font-Lalezar text-2xl text-center">پنل مدیریت ادمین</h1>
-        <ul className="mt-5 space-y-1.5">
+        <ul className="mt-5 space-y-0.5">
           {adminPanelLinks.map((link) => (
             <MenuItem {...link} key={link.id} />
           ))}
