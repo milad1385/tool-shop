071799c00e17dc38e2ccbd7715c6c110f17c61ca
Milad salamian <miladsalami1385@gmail.com>
2025-09-28 13:55:46 -0700

    change option arr props in comment page in p-admin

--- src/app/p-admin/comments/page.tsx
+++ src/app/p-admin/comments/page.tsx
@@ -2,16 +2,13 @@ import Container from "@/components/modules/p-admin/Container";
 import PageTitle from "@/components/modules/p-admin/PageTitle";
 import TableOperation from "@/components/modules/p-admin/TableOpration";
 import CommentList from "@/components/templates/p-admin/CommentList";
-import { CommentFilterOptions } from "@/constants/data";
+import { statusFilterOptions } from "@/constants/data";
 import React from "react";
 
 function page() {
   return (
     <Container>
-      <TableOperation
-        options={CommentFilterOptions}
-        pageTitle="لیست کامنت ها"
-      />
+      <TableOperation options={statusFilterOptions} pageTitle="لیست کامنت ها" />
       <CommentList />
     </Container>
   );
