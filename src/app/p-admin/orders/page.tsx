2d2c42fe6b787b7ae11ab1333263ab38552df8f8
Milad salamian <miladsalami1385@gmail.com>
2025-11-14 05:29:10 -0800

    use order list component in order page at admin panel

--- src/app/p-admin/orders/page.tsx
+++ src/app/p-admin/orders/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/components/modules/p-admin/Container";
 import PageTitle from "@/components/modules/p-admin/PageTitle";
 import TableOperation from "@/components/modules/p-admin/TableOpration";
+import OrderList from "@/components/templates/p-admin/OrderList";
 import { orderStatusFilterOptions } from "@/constants/data";
 import React from "react";
 
@@ -8,6 +9,7 @@ function page() {
   return (
     <Container>
      <TableOperation pageTitle="لیست سفارش ها" options={orderStatusFilterOptions} />
+     <OrderList/>
     </Container>
   );
 }
