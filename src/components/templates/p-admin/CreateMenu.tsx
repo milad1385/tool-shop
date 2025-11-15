f7af763dd58550c1d99e7b2db111528c016c50b9
Milad salamian <miladsalami1385@gmail.com>
2025-11-15 02:32:42 -0800

    add create menu component

---
+++ src/components/templates/p-admin/CreateMenu.tsx
@@ -0,0 +1,132 @@
+"use client";
+import Button from "@/components/ui/Button";
+import Input from "@/components/ui/Input";
+import { createMenuSchema, TMenuSchema } from "@/validators/frontend/menu.validator";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import Image from "next/image";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaRegTrashAlt } from "react-icons/fa";
+
+function CreateMenu() {
+  const [image, setImage] = useState<any>(null);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    setValue,
+    reset,
+  } = useForm({
+    resolver: yupResolver(createMenuSchema),
+  });
+
+  const createMenuHandler = (data: TMenuSchema) => {
+    console.log(data);
+  };
+  return (
+    <form
+      onSubmit={handleSubmit(createMenuHandler)}
+      className="section-box"
+    >
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-7">
+        <Input
+          register={register}
+          errors={errors}
+          name="name"
+          type="text"
+          label="نام"
+          className="bg-gray-50"
+          disable={false}
+          labelClassName="!text-lg font-Iran"
+        />
+        <Input
+          register={register}
+          errors={errors}
+          name="href"
+          type="text"
+          label="لینک"
+          className="bg-gray-50"
+          disable={false}
+          labelClassName="!text-lg font-Iran"
+        />
+        
+        <Input
+          register={register}
+          errors={errors}
+          name="desc"
+          type="text"
+          label="توضیحات"
+          className="bg-gray-50"
+          disable={false}
+          labelClassName="!text-lg font-Iran"
+        />
+
+        <Input
+          register={register}
+          errors={errors}
+          name="parentMenu"
+          type="select"
+          className="!bg-gray-50"
+          options={[
+            { id: 1, label: "منو اول", value: "menu1" },
+            { id: 2, label: "منو دوم", value: "menu2" },
+          ]}
+          label="منو پرنت"
+          disable={false}
+          labelClassName="!text-lg font-Iran"
+        />
+     
+        <Input
+          register={register}
+          errors={errors}
+          name="image"
+          type="file"
+          label="آیکون"
+          disable={false}
+          labelClassName="!text-lg font-Iran"
+          setImage={setImage}
+        />
+
+        {image && (
+          <div className="flex items-end justify-end">
+            <div className="relative">
+              <Image
+                src={image}
+                width={1920}
+                height={1080}
+                className="w-[200px] rounded-md"
+                alt="image"
+              />
+
+              <FaRegTrashAlt
+                onClick={() => {
+                  setImage("");
+                  setValue("image", "");
+                }}
+                className="text-red-500 absolute -top-8 right-0 text-xl md:cursor-pointer"
+              />
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="flex items-center gap-x-4">
+        <Button type="submit" className="!w-[200px] mt-10">
+          ایجاد منو
+        </Button>
+        <Button
+          onClick={() => {
+            reset();
+            setImage("");
+          }}
+          type="reset"
+          className="!w-[200px] mt-10 !bg-red-500"
+        >
+          لغو
+        </Button>
+      </div>
+    </form>
+  );
+}
+
+export default CreateMenu;
