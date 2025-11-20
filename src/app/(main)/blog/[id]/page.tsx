0a15bbe44811e59b98a6600fee28d3380846553a
Milad salamian <miladsalami1385@gmail.com>
2025-11-20 04:34:01 -0800

    add suspense in blog page

--- src/app/(main)/blog/[id]/page.tsx
+++ src/app/(main)/blog/[id]/page.tsx
@@ -6,6 +6,7 @@ import ArticleDeatils from "@/components/templates/articles/ArticleDeatils";
 import ArticleTitle from "@/components/templates/articles/ArticleTitle";
 import RecentArticles from "@/components/templates/articles/RecentArticles";
 import { IPage } from "@/libs/types";
+import { Suspense } from "react";
 
 async function page({ params }: IPage) {
   const { id } = await params;
@@ -19,17 +20,19 @@ async function page({ params }: IPage) {
           { id: 3, href: "/blog/1", name: "مقاله راجع فلان" },
         ]}
       />
-      <div className="grid grid-cols-12 gap-4">
-        <div className="col-span-12 md:col-span-9  bg-white p-6 rounded-3xl leading-8">
-          <ArticleTitle />
-          <ArticleDeatils />
-          <ArticleComments />
+      <Suspense fallback={<div>Loading ...</div>}>
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-12 md:col-span-9  bg-white p-6 rounded-3xl leading-8">
+            <ArticleTitle />
+            <ArticleDeatils />
+            <ArticleComments />
+          </div>
+          <div className="col-span-12 md:col-span-3">
+            <RecentArticles />
+            <ArticleCategories />
+          </div>
         </div>
-        <div className="col-span-12 md:col-span-3">
-          <RecentArticles />
-          <ArticleCategories />
-        </div>
-      </div>
+      </Suspense>
     </Container>
   );
 }
