018b696bd0c777c0da5d6abe1afa8d0129fbe963
Milad salamian <miladsalami1385@gmail.com>
2025-11-20 05:01:39 -0800

    fix pagination fallback bug

--- src/components/templates/products/Products.tsx
+++ src/components/templates/products/Products.tsx
@@ -1,25 +1,28 @@
 import Pagination from "@/components/modules/main/Pagination";
 import ProductBox from "@/components/modules/main/ProductBox";
 import { products } from "@/constants/data";
-import React from "react";
+import React, { Suspense } from "react";
 import SortProduct from "./SortProduct";
 import MobileSearch from "./MobileSearch";
 import MobileFilter from "./MobileFilter";
+import PaginationFallback from "@/components/modules/main/PaginationFallback";
 
 function Products() {
   return (
     <div className="col-span-12 md:col-span-9">
-      <MobileSearch/>
-      <MobileFilter/>
-      <SortProduct/>
+      <MobileSearch />
+      <MobileFilter />
+      <SortProduct />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
         {products.map((product) => (
           <ProductBox {...product} key={product.id} />
         ))}
       </div>
-      <div className="w-full">
-        <Pagination count={4} />
-      </div>
+      <Suspense fallback={<PaginationFallback />}>
+        <div className="w-full">
+          <Pagination count={4} />
+        </div>
+      </Suspense>
     </div>
   );
 }
