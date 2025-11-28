855994bf3ef9b49392ac36008a61d87d611a3595
Milad salamian <miladsalami1385@gmail.com>
2025-11-28 12:02:38 -0800

    delete unneccery imports

--- src/app/p-admin/articles/page.tsx
+++ src/app/p-admin/articles/page.tsx
@@ -1,9 +1,7 @@
 import Container from "@/components/modules/p-admin/Container";
-import PageTitle from "@/components/modules/p-admin/PageTitle";
 import TableOperation from "@/components/modules/p-admin/TableOpration";
 import ArticleList from "@/components/templates/p-admin/ArticleList";
 import { articleFilter } from "@/constants/data";
-import React from "react";
 
 function page() {
   return (
