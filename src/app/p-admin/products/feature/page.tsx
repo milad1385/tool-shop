8637e1f240e502d63039750ca51d39180712508c
Milad salamian <miladsalami1385@gmail.com>
2025-11-28 12:28:09 -0800

    use product feature list in p-admin

--- src/app/p-admin/products/feature/page.tsx
+++ src/app/p-admin/products/feature/page.tsx
@@ -1,6 +1,8 @@
 import Container from "@/components/modules/p-admin/Container";
 import PageTitle from "@/components/modules/p-admin/PageTitle";
+import TableOperation from "@/components/modules/p-admin/TableOpration";
 import CreateProductFeature from "@/components/templates/p-admin/CreateProductFeature";
+import ProductFeatureList from "@/components/templates/p-admin/ProductFeatureList";
 import React from "react";
 
 function page() {
@@ -8,6 +10,8 @@ function page() {
     <Container>
       <PageTitle content="ایجاد ویژگی محصول" />
       <CreateProductFeature />
+      <TableOperation pageTitle="لیست ویژگی ها" />
+      <ProductFeatureList/>
     </Container>
   );
 }
