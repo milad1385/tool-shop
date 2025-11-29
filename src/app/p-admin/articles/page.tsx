89cce9b5a1b956df713c24c5ee63bf7617ba315f
Milad salamian <miladsalami1385@gmail.com>
2025-11-29 13:42:56 -0800

    use create new article component in p-admin

--- src/app/p-admin/articles/page.tsx
+++ src/app/p-admin/articles/page.tsx
@@ -2,10 +2,14 @@ import Container from "@/components/modules/p-admin/Container";
 import TableOperation from "@/components/modules/p-admin/TableOpration";
 import ArticleList from "@/components/templates/p-admin/ArticleList";
 import { articleFilter } from "@/constants/data";
+import PageTitle from "../../../components/modules/p-admin/PageTitle";
+import CreateNewArticle from "@/components/templates/p-admin/CreateNewArticle";
 
 function page() {
   return (
     <Container>
+      <PageTitle content="ایجاد مقاله" />
+      <CreateNewArticle />
       <TableOperation pageTitle="لیست مقاله ها" options={articleFilter} />
       <ArticleList />
     </Container>
