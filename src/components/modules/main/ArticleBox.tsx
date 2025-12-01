4bc4b5b08767a62ac7fd083ea1df4244045b4058
Milad salamian <miladsalami1385@gmail.com>
2025-12-01 01:31:10 -0800

    fix some style bug in article box component

--- src/components/modules/main/ArticleBox.tsx
+++ src/components/modules/main/ArticleBox.tsx
@@ -5,15 +5,18 @@ import Link from "next/link";
 import React from "react";
 import { IoArrowBack } from "react-icons/io5";
 
-function ArticleBox({image, link, title }: IArticle) {
+function ArticleBox({ image, link, title }: IArticle) {
   return (
     <div className="bg-white shadow rounded-3xl p-4 overflow-hidden">
-      <Link href={link} className="block overflow-hidden group relative rounded-3xl">
+      <Link
+        href={link}
+        className="block overflow-hidden group relative rounded-3xl"
+      >
         <div className="flex flex-col  absolute z-20 top-4 left-4 bg-white border-t-4 border-yellow-400 p-2 px-3 rounded-xl">
-          <span className="font-IranMedium font-bold text-2xl">
+          <span className="font-IranMedium font-bold text-lg md:text-2xl">
             {formattedPrice(26)}
           </span>
-          <span className="font-IranMedium text-sm">خرداد</span>
+          <span className="font-IranMedium text-xs md:text-sm">خرداد</span>
         </div>
 
         <div className="bg-black/50 absolute inset-0 flex-center opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all md:cursor-pointer ">
@@ -31,11 +34,12 @@ function ArticleBox({image, link, title }: IArticle) {
           height={1080}
           src={`/images/${image}`}
           alt="blog-1"
+          className="h-[200px] md:h-[260px] object-cover"
         />
       </Link>
       <Link href={link} className="flex items-center justify-between mt-3">
-        <h3 className="font-Lalezar text-lg">{title}</h3>
-        <div className="flex items-center gap-x-2">
+        <h3 className="font-Lalezar text-base md:text-lg">{title}</h3>
+        <div className="flex items-center gap-x-2 text-sm md:text-lg">
           <span>بیشتر</span>
           <IoArrowBack className="text-lg" />
         </div>
