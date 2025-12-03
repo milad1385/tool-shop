11bf0ee08a3fd95c6d1a7a3c93463ded36178cd2
Milad salamian <miladsalami1385@gmail.com>
2025-12-03 14:14:49 -0800

    fix overlay bug

--- src/components/modules/main/Overlay.tsx
+++ src/components/modules/main/Overlay.tsx
@@ -10,7 +10,7 @@ function MenuOverlay({ isOpen, onClose, isMobile }: TOverlay) {
   return (
     <div
       onClick={() => onClose(false)}
-      className={`fixed md:hidden inset-0 ${isMobile ? "z-30" : "z-20"} z-20 ${
+      className={`fixed md:hidden inset-0 ${isMobile ? "z-40" : "z-20"} z-20 ${
         isOpen ? "opacity-100 visible" : "opacity-0 invisible"
       } bg-black/30 transition-all`}
     ></div>
