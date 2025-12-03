81e16671ab1713126cf61e15b724fe3f2d64e0fd
Milad salamian <miladsalami1385@gmail.com>
2025-12-03 14:28:25 -0800

    fix color box style bug

--- src/components/modules/main/ColorBox.tsx
+++ src/components/modules/main/ColorBox.tsx
@@ -7,7 +7,7 @@ function ColorBox({ label, slug, onClick , selected}: IColorBox) {
       onClick={() => onClick?.(slug)}
       className={`px-2 py-1.5 inline-flex items-center gap-x-3 border border-gray-400 rounded-lg md:cursor-pointer ${selected ?"bg-yellow-200 border-yellow-500" :""}`}
     >
-      <span>{label}</span>
+      <span className="text-sm md:text-base">{label}</span>
       <span
         className={`block w-4 h-4 rounded-full ${
           slug.includes("black") ? "bg-black" : `bg-${slug}-500`
