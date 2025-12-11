c85675f240175018dad8e8078f3773527d930947
Milad salamian <miladsalami1385@gmail.com>
2025-12-11 06:50:28 -0800

    use create slider component in setting page of p-admin

--- src/app/p-admin/settings/page.tsx
+++ src/app/p-admin/settings/page.tsx
@@ -1,11 +1,13 @@
 import Container from "@/components/modules/p-admin/Container";
 import PageTitle from "@/components/modules/p-admin/PageTitle";
+import CreateSlider from "@/components/templates/p-admin/CreateSlider";
 import React from "react";
 
 function page() {
   return (
     <Container>
-      <PageTitle content="لیست تنظیمات" />
+      <PageTitle content="تنظیمات سایت ترازو" />
+      <CreateSlider />
     </Container>
   );
 }
--- src/components/modules/p-admin/PageTitle.tsx
+++ src/components/modules/p-admin/PageTitle.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
-function PageTitle({ content }: { content: string }) {
+function PageTitle({
+  content,
+  className,
+}: {
+  content: string;
+  className?: string;
+}) {
   const text = content.split(" ");
   return (
-    <h2 className="text-lg md:text-xl lg:text-3xl pb-2 font-Lalezar text-zinc-700 flex items-center gap-x-2">
+    <h2
+      className={`text-lg md:text-xl lg:text-3xl pb-2 font-Lalezar text-zinc-700 flex items-center gap-x-2 ${className}`}
+    >
       <span>{text[0]}</span>
       <span className="text-yellow-500">
         {text[1]} {text?.[2]} {text?.[3]}
