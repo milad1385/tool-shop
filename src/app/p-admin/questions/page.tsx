30fa01c1096a583bd7966a22a173ced0ad1303a6
Milad salamian <miladsalami1385@gmail.com>
2025-12-12 05:59:52 -0800

    use question list component in question page

--- src/app/p-admin/questions/page.tsx
+++ src/app/p-admin/questions/page.tsx
@@ -2,14 +2,16 @@ import Container from "@/components/modules/p-admin/Container";
 import PageTitle from "@/components/modules/p-admin/PageTitle";
 import TableOperation from "@/components/modules/p-admin/TableOpration";
 import CreateQuestion from "@/components/templates/p-admin/CreateQuestion";
+import QuestionList from "@/components/templates/p-admin/QuestionList";
 import { questionFilterOptions } from "@/constants/data";
 
 function page() {
   return (
     <Container>
       <PageTitle content="ایجاد سوال جدید" />
-      <CreateQuestion/>
-      <TableOperation pageTitle="لیست سوالات" options={questionFilterOptions}/>
+      <CreateQuestion />
+      <TableOperation pageTitle="لیست سوالات" options={questionFilterOptions} />
+      <QuestionList />
     </Container>
   );
 }
