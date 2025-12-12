63773a92de2287f3a6348d8dee35a85e4f21c069
Milad salamian <miladsalami1385@gmail.com>
2025-12-12 05:31:26 -0800

    add new info validator schema

--- src/validators/frontend/settings.validator.ts
+++ src/validators/frontend/settings.validator.ts
@@ -31,5 +31,43 @@ export const sliderSchema = yup.object({
     .max(100, "حداکثر تعداد لینک اسلایدر 100 کاراکتر عدد میباشد"),
   order: yup.string().required("وارد کردن اولویت اسلایدر الزامی است"),
 });
+export const siteInfoSchema = yup.object({
+  address: yup
+    .string()
+    .required("وارد کردن آدرس الزامی است")
+    .min(3, "حداقل 3 کاراکتر باید وارد کنید")
+    .max(2000, "حداکثر 2000 کاراکتر باید وارد کنید"),
+  phone: yup
+    .string()
+    .required("وارد کردن شماره ثابت الزامی است")
+    .min(11, "حداقل 11 کاراکتر وارد کنید")
+    .max(11, "حداکثر 11 کاراکتر وارد کنید"),
+  mobilePhone: yup
+    .string()
+    .required("وارد کردن شماره ثابت الزامی است")
+    .min(11, "حداقل 11 کاراکتر وارد کنید")
+    .max(11, "حداکثر 11 کاراکتر وارد کنید"),
+  instagram: yup
+    .string()
+    .required("وارد کردن آیدی اینستاگرام الزامی است")
+    .min(5, "حداقل 5 کاراکتر باید وارد کنید")
+    .max(50, "حداکثر 50 کاراکتر باید وارد کنید"),
+  telegram: yup
+    .string()
+    .required("وارد کردن آیدی تلگرام الزامی است")
+    .min(5, "حداقل 5 کاراکتر باید وارد کنید")
+    .max(50, "حداکثر 50 کاراکتر باید وارد کنید"),
+  description: yup
+    .string()
+    .required("وارد کردن توضیحات فوتر الزامی است")
+    .min(50, "حداقل 50 کاراکتر باید وارد کنید")
+    .max(2000, "حداکثر 2000 کاراکتر باید وارد کنید"),
+  email: yup
+    .string()
+    .email("ایمیل معتبر وارد کنید")
+    .required("وارد کردن ایمیل الزامی است")
+    .max(100,"حداکثر کاراکتر ایمیل 100 تا میباشد"),
+});
 
 export type TSliderSchema = yup.InferType<typeof sliderSchema>;
+export type TSiteInfoSchema = yup.InferType<typeof siteInfoSchema>;
