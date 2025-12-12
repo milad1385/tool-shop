f26f0e2a4bfdf547e15ef7552abe8263f5635717
Milad salamian <miladsalami1385@gmail.com>
2025-12-12 04:59:58 -0800

    use create slider in slider page of p-admin

--- src/app/p-admin/settings/page.tsx
+++ src/app/p-admin/settings/page.tsx
@@ -1,13 +1,10 @@
 import Container from "@/components/modules/p-admin/Container";
 import PageTitle from "@/components/modules/p-admin/PageTitle";
-import CreateSlider from "@/components/templates/p-admin/CreateSlider";
-import React from "react";
 
 function page() {
   return (
     <Container>
       <PageTitle content="تنظیمات سایت ترازو" />
-      <CreateSlider />
     </Container>
   );
 }
