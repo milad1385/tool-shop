3cc645c9ee3a96ddc186e46a0766ec13861d39a7
Milad salamian <miladsalami1385@gmail.com>
2025-12-12 04:59:02 -0800

    add slider page

---
+++ src/app/p-admin/sliders/page.tsx
@@ -0,0 +1,19 @@
+import Container from "@/components/modules/p-admin/Container";
+import PageTitle from "@/components/modules/p-admin/PageTitle";
+import TableOperation from "@/components/modules/p-admin/TableOpration";
+import CreateSlider from "@/components/templates/p-admin/CreateSlider";
+import SliderList from "@/components/templates/p-admin/SliderList";
+import { sliderFilterOptions } from "@/constants/data";
+
+function page() {
+  return (
+    <Container>
+      <PageTitle content="ایجاد اسلایدر جدید" />
+      <CreateSlider/>
+      <TableOperation pageTitle="لیست اسلایدر ها" options={sliderFilterOptions}/>
+      <SliderList/>
+    </Container>
+  );
+}
+
+export default page;
