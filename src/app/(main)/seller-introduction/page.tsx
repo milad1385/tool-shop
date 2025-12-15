3cdbf5379e663b2be1c4718f97a87d5ba225f691
Milad salamian <miladsalami1385@gmail.com>
2025-12-15 09:22:20 -0800

    add seller introduction page

---
+++ src/app/(main)/seller-introduction/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/templates/seller-introduction/Navbar";
+import SellerBanner from "@/components/templates/seller-introduction/SellerBanner";
+
+function page() {
+  return (
+    <div>
+      <Navbar/>
+      <SellerBanner/>
+    </div>
+  );
+}
+
+export default page;
