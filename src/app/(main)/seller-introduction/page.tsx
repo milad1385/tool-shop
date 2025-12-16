487ad5faa959e581e1621b2c091474040ed242e8
Milad salamian <miladsalami1385@gmail.com>
2025-12-16 11:48:27 -0800

    use why trazo component in target page

--- src/app/(main)/seller-introduction/page.tsx
+++ src/app/(main)/seller-introduction/page.tsx
@@ -1,11 +1,16 @@
+import Container from "@/components/modules/main/Container";
 import Navbar from "@/components/templates/seller-introduction/Navbar";
 import SellerBanner from "@/components/templates/seller-introduction/SellerBanner";
+import WhyTrazo from "@/components/templates/seller-introduction/WhyTrazo";
 
 function page() {
   return (
     <div>
       <Navbar />
       <SellerBanner />
+      <Container>
+        <WhyTrazo/>
+      </Container>
     </div>
   );
 }
