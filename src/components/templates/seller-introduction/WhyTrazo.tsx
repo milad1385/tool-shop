c4d6b3188a40741b6b1891370d8f0eebefb7fe53
Milad salamian <miladsalami1385@gmail.com>
2025-12-16 11:47:50 -0800

    add why trazo component

---
+++ src/components/templates/seller-introduction/WhyTrazo.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import TrazoItems from "./TrazoItems";
+
+function WhyTrazo() {
+  return (
+    <div className="mt-[450px]">
+      <h3 className="text-xl font-bold text-zinc-700">
+        چرا ترازو جای خوبی برای فروش کالاست؟
+      </h3>
+      <span className="block h-[2.35px] bg-yellow-500 w-28 mt-5"></span>
+
+      <TrazoItems />
+    </div>
+  );
+}
+
+export default WhyTrazo;
