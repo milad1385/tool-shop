c24ffa47240b2043f8c18188a1f4162a897947c6
Milad salamian <miladsalami1385@gmail.com>
2025-12-16 11:47:40 -0800

    add trazo items component

---
+++ src/components/templates/seller-introduction/TrazoItems.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TrazoItem from "./TrazoItem";
+import { trazoItems } from "@/constants/data";
+
+function TrazoItems() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
+      {trazoItems.map((trazo) => (
+        <TrazoItem {...trazo} key={trazo.id} />
+      ))}
+    </div>
+  );
+}
+
+export default TrazoItems;
