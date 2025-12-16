111626403026b30361850db241dca48078aec001
Milad salamian <miladsalami1385@gmail.com>
2025-12-16 10:43:00 -0800

    add resposive layout in seller introduction page

--- src/components/templates/seller-introduction/Navbar.tsx
+++ src/components/templates/seller-introduction/Navbar.tsx
@@ -15,10 +15,10 @@ function Navbar() {
         />
       </div>
       <div className="flex items-center gap-x-4">
-        <Link href="/" className="text-sm text-white">
+        <Link href="/" className="text-xs md:text-sm text-white">
           ورود به پنل فروشندگان
         </Link>
-        <button className="px-16 py-2 border-2 text-white border-white rounded-lg text-sm">
+        <button className="px-6 md:px-16 py-2 border-2 text-white border-white rounded-lg text-sm">
           ثبت نام
         </button>
       </div>
--- src/components/templates/seller-introduction/SellerBanner.tsx
+++ src/components/templates/seller-introduction/SellerBanner.tsx
@@ -7,7 +7,7 @@ import Typewriter from "typewriter-effect";
 function SellerBanner() {
   return (
     <div className="relative">
-      <div className="absolute inset-0 bg-black/15"></div>
+      <div className="absolute inset-0 bg-black/25"></div>
       <Image
         alt="seller-introduction"
         width={1920}
@@ -15,11 +15,21 @@ function SellerBanner() {
         src="/images/seller-banner.png"
         placeholder="blur"
         blurDataURL="/images/seller-banner.png"
-        className="h-[110vh] mt-20"
+        className="h-[110vh] mt-20 hidden md:block object-cover"
       />
 
-      <div className="absolute z-30 top-80 left-8 space-y-6 w-[450px]">
-        <h3 className="text-4xl font-bold text-white">
+      <Image
+        alt="seller-introduction"
+        width={1920}
+        height={1080}
+        src="/images/seller-mobile.jpg"
+        placeholder="blur"
+        blurDataURL="/images/seller-mobile.jpg"
+        className="h-[80vh] mt-20 block md:hidden object-cover"
+      />
+
+      <div className="absolute z-30 top-80 right-5 md:left-8 space-y-6 w-[450px]">
+        <h3 className="text-2xl md:text-4xl font-bold text-white">
           در ترازو فروشنده شوید !
         </h3>
 
@@ -38,7 +48,12 @@ function SellerBanner() {
           }}
         />
 
-        <Link href="/" className="block px-6 bg-black text-white w-[160px] py-3 text-center text-sm rounded-lg">ثبت نام فروشنده</Link>
+        <Link
+          href="/"
+          className="block px-6 bg-yellow-600 md:bg-black text-white w-[160px] py-3 text-center text-sm rounded-lg"
+        >
+          ثبت نام فروشنده
+        </Link>
       </div>
     </div>
   );
