fc98ec98be86e2df19d83dae0606bdeb48121793
Milad salamian <miladsalami1385@gmail.com>
2025-12-18 14:12:10 -0800

    add tax category component

---
+++ src/components/templates/seller-introduction/TaxCategory.tsx
@@ -0,0 +1,24 @@
+import { ITaxCategory } from "@/libs/types";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function TaxCategory({ id, image, link, max, min, title }: ITaxCategory) {
+  return (
+    <Link key={id} className="space-y-3" href={link}>
+      <Image
+        alt="image"
+        width={1920}
+        height={1080}
+        src={`/images/${image}`}
+        className="w-[50px]"
+      />
+      <p className="font-bold text-sm md:text-base">{title}</p>
+      <p className="text-xs font-bold text-zinc-500">
+        از {min} ٪ تا {max} ٪ کمیسیون
+      </p>
+    </Link>
+  );
+}
+
+export default TaxCategory;
