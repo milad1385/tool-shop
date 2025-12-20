9151a483b12ed409532fa3e371a453191edb1f1a
Milad salamian <miladsalami1385@gmail.com>
2025-12-20 12:16:39 -0800

    add sellers comment component

---
+++ src/components/templates/seller-introduction/SellersComment.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import SellersCommentList from "./SellersCommentList";
+import Title from "./Title";
+
+function SellersComment() {
+  return (
+    <div>
+      <Title content="داستان‌های موفقیت از زبان فروشندگان" />
+      <div className="grid grid-cols-12 mt-8">
+        <div className="col-span-7 border border-gray-300 p-2 rounded-xl">
+          <Image
+            alt="image"
+            src="/images/blog-3.jpg"
+            width={1920}
+            height={1080}
+            className="w-[700px] h-[390px] rounded-md"
+          />
+        </div>
+        <div className="col-span-5 flex items-center justify-center">
+            <SellersCommentList/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SellersComment;
