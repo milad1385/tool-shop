63b0f2ef9d64b532c727f966debb20e6dafd5b07
Milad salamian <miladsalami1385@gmail.com>
2025-12-26 08:38:41 -0800

    use seller link component in seller side bar

--- src/components/templates/seller-introduction/Sidebar.tsx
+++ src/components/templates/seller-introduction/Sidebar.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { sideBarLinks } from "@/constants/data";
+import SellerStepLink from "./SellerStepLink";
 
 function Sidebar() {
   return (
-    <div className='flex-[0.32] bg-gray-200 min-h-screen'>Sidebar</div>
-  )
+    <div className="flex-[0.32] bg-gray-200 min-h-screen pr-5 py-12 space-y-4">
+      {sideBarLinks.map((link) => (
+        <SellerStepLink
+          key={link.id}
+          title={link.title}
+          isActive={link.isActive}
+          icon={link.icon}
+          options={link.options}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
