e4e751a8ac4bc2eccbd442460b74e81bdcdb79c6
Milad salamian <miladsalami1385@gmail.com>
2026-01-07 14:19:41 -0800

    fix seller register side bar bug

--- src/components/templates/seller-introduction/SellerRegisterContainer.tsx
+++ src/components/templates/seller-introduction/SellerRegisterContainer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function SellerRegisterContainer({ children }: { children: React.ReactNode }) {
-  return <div className="flex-1 md:flex-[0.8] md:px-56 py-20">{children}</div>;
+  return <div className="flex-1 md:flex-[0.8] md:px-10 lg:px-56 py-20">{children}</div>;
 }
 
 export default SellerRegisterContainer;
