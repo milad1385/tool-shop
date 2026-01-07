6b98ed2bd085f9a0cc0edb6771766b8c7cf1eb17
Milad salamian <miladsalami1385@gmail.com>
2026-01-07 14:14:44 -0800

    add seller register container

---
+++ src/components/templates/seller-introduction/SellerRegisterContainer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SellerRegisterContainer({ children }: { children: React.ReactNode }) {
+  return <div className="flex-1 md:flex-[0.8] md:px-56 py-20">{children}</div>;
+}
+
+export default SellerRegisterContainer;
