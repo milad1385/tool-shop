43a52c07b8c7ac1cd4ecedfc71fef315a9bdc3c6
Milad salamian <miladsalami1385@gmail.com>
2026-01-07 14:39:46 -0800

    fix style bug in seller register page

--- src/components/templates/seller-introduction/CancelSellerRegisteration.tsx
+++ src/components/templates/seller-introduction/CancelSellerRegisteration.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function CancelSellerRegisteration() {
   return (
-    <div className="flex-center py-10">
+    <div className="flex-center pt-7">
       <Link href="/seller-introduction" className="text-zinc-700 block">
         انصراف از ثبت نام
       </Link>
--- src/components/templates/seller-introduction/SellerStatus.tsx
+++ src/components/templates/seller-introduction/SellerStatus.tsx
@@ -1,19 +1,21 @@
 import { sellerStatusBoxes } from "@/constants/data";
+import CancelSellerRegisteration from "./CancelSellerRegisteration";
+import MobileMenuBar from "./MobileMenuBar";
 import RegisterTitle from "./RegisterTitle";
 import SellerRegisterContainer from "./SellerRegisterContainer";
 import SellerStatusBox from "./SellerStatusBox";
-import MobileMenuBar from "./MobileMenuBar";
 
 function SellerStatus() {
   return (
     <SellerRegisterContainer>
       <RegisterTitle content="وضعیت فروشنده را انتخاب کنید" />
-      <MobileMenuBar/>
+      <MobileMenuBar />
       <div className="flex flex-col md:flex-row items-center gap-5 mt-16">
         {sellerStatusBoxes.map((sellerStatusBox) => (
           <SellerStatusBox {...sellerStatusBox} key={sellerStatusBox.id} />
         ))}
       </div>
+      <CancelSellerRegisteration/>
     </SellerRegisterContainer>
   );
 }
