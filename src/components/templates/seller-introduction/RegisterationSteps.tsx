c1c0663e0849ee88213187462848ab26f6e3eedc
Milad salamian <miladsalami1385@gmail.com>
2026-02-04 01:33:17 +0330

    delete unneccery imports

--- src/components/templates/seller-introduction/RegisterationSteps.tsx
+++ src/components/templates/seller-introduction/RegisterationSteps.tsx
@@ -1,6 +1,5 @@
 import CancelSellerRegisteration from "./CancelSellerRegisteration";
 import MobileMenuBar from "./MobileMenuBar";
-import RegisterTitle from "./RegisterTitle";
 import SellerRegisterContainer from "./SellerRegisterContainer";
 import SellerStatus from "./SellerStatus";
 
