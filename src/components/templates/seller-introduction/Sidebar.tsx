45fecedaeb23e920316207fe25d8e1e02ac3eae2
Milad salamian <miladsalami1385@gmail.com>
2026-02-04 03:12:10 +0330

    handle active status stepper in sidebar

--- src/components/templates/seller-introduction/Sidebar.tsx
+++ src/components/templates/seller-introduction/Sidebar.tsx
@@ -1,10 +1,46 @@
+"use client";
+
 import { sideBarLinks } from "@/constants/data";
 import SellerStepLink from "./SellerStepLink";
+import { useSearchParams } from "next/navigation";
 
 function Sidebar() {
+  const searchParams = useSearchParams();
+
+  const step = Number(searchParams.get("step")) || 1;
+
+  const subStep = Number(searchParams.get("substep")) || 1;
+
+  const updatedLinks = sideBarLinks.map((link) => {
+    const isStepActive = link.id <= step;
+
+    return {
+      ...link,
+      isActive: isStepActive,
+      options: link.options.map((opt) => {
+        if (link.id === step) {
+          return {
+            ...opt,
+            isActive: opt.id <= subStep,
+          };
+        }
+        if (link.id < step) {
+          return {
+            ...opt,
+            isActive: true,
+          };
+        }
+        return {
+          ...opt,
+          isActive: false,
+        };
+      }),
+    };
+  });
+
   return (
     <div className="hidden lg:block lg:w-[300px] bg-gray-200 min-h-screen pr-5 py-12 space-y-4">
-      {sideBarLinks.map((link) => (
+      {updatedLinks.map((link) => (
         <SellerStepLink
           key={link.id}
           title={link.title}
