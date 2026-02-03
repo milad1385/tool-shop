4d2cd0b512f5252261e8e1c019862b1438f9877f
Milad salamian <miladsalami1385@gmail.com>
2026-02-03 12:00:44 +0330

    add seller status component

--- src/components/templates/seller-introduction/SellerStatus.tsx
+++ src/components/templates/seller-introduction/SellerStatus.tsx
@@ -1,22 +1,13 @@
 import { sellerStatusBoxes } from "@/constants/data";
-import CancelSellerRegisteration from "./CancelSellerRegisteration";
-import MobileMenuBar from "./MobileMenuBar";
-import RegisterTitle from "./RegisterTitle";
-import SellerRegisterContainer from "./SellerRegisterContainer";
 import SellerStatusBox from "./SellerStatusBox";
 
 function SellerStatus() {
   return (
-    <SellerRegisterContainer>
-      <RegisterTitle content="وضعیت فروشنده را انتخاب کنید" />
-      <MobileMenuBar />
-      <div className="flex flex-col md:flex-row items-center gap-5 mt-16">
-        {sellerStatusBoxes.map((sellerStatusBox) => (
-          <SellerStatusBox {...sellerStatusBox} key={sellerStatusBox.id} />
-        ))}
-      </div>
-      <CancelSellerRegisteration/>
-    </SellerRegisterContainer>
+    <div className="flex flex-col md:flex-row items-center gap-5 mt-16">
+      {sellerStatusBoxes.map((sellerStatusBox) => (
+        <SellerStatusBox {...sellerStatusBox} key={sellerStatusBox.id} />
+      ))}
+    </div>
   );
 }
 
