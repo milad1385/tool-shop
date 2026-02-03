5a295c4018f3f2d5f301132d01bce24cf6013b7a
Milad salamian <miladsalami1385@gmail.com>
2026-02-04 03:11:53 +0330

    handle stepper form

--- src/components/templates/seller-introduction/RegisterationSteps.tsx
+++ src/components/templates/seller-introduction/RegisterationSteps.tsx
@@ -1,16 +1,34 @@
+"use client";
+import { useSearchParams } from "next/navigation";
 import CancelSellerRegisteration from "./CancelSellerRegisteration";
 import MobileMenuBar from "./MobileMenuBar";
+import SellerInformation from "./SellerInformation";
 import SellerRegisterContainer from "./SellerRegisterContainer";
 import SellerStatus from "./SellerStatus";
 
 function RegisterationSteps() {
+  const searchParam = useSearchParams();
+
+
+  const step = Number(searchParam.get("step")) || 1;
+  const subStep = Number(searchParam.get("substep")) || 1;
+ 
+
   return (
     <SellerRegisterContainer>
       <MobileMenuBar />
-      <SellerStatus />
+      {renderStep(+step, +subStep)}
       <CancelSellerRegisteration />
     </SellerRegisterContainer>
   );
+}
+
+function renderStep(step: number, subStep: number) {
+  if (step === 1 && subStep === 1) {
+    return <SellerStatus />;
+  } else if (step === 1 && subStep === 2) {
+    return <SellerInformation />;
+  }
 }
 
 export default RegisterationSteps;
