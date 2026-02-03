c73e4ee161da4e19952ee77263b3978677692883
Milad salamian <miladsalami1385@gmail.com>
2026-02-03 12:01:34 +0330

    delete some unimportnat import

--- src/components/templates/seller-introduction/RegisterationSteps.tsx
+++ src/components/templates/seller-introduction/RegisterationSteps.tsx
@@ -1,9 +1,7 @@
-import { sellerStatusBoxes } from "@/constants/data";
 import CancelSellerRegisteration from "./CancelSellerRegisteration";
 import MobileMenuBar from "./MobileMenuBar";
 import RegisterTitle from "./RegisterTitle";
 import SellerRegisterContainer from "./SellerRegisterContainer";
-import SellerStatusBox from "./SellerStatusBox";
 import SellerStatus from "./SellerStatus";
 
 function RegisterationSteps() {
