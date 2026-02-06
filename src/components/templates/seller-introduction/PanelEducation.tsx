ebf240232341bbf68c401a133f073af80ffb9791
Milad salamian <miladsalami1385@gmail.com>
2026-02-07 02:27:20 +0330

    add penel education component

---
+++ src/components/templates/seller-introduction/PanelEducation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RegisterTitle from "./RegisterTitle";
+
+function PanelEducation() {
+  return (
+    <div>
+      <RegisterTitle content="آشنایی نحوه کار کردن با پنل فروشنده" />
+    </div>
+  );
+}
+
+export default PanelEducation;
