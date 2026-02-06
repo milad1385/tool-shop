5008509674cf774109f99591adaffaec90117cf2
Milad salamian <miladsalami1385@gmail.com>
2026-02-07 01:43:22 +0330

    add seller location choose step in registeration form

---
+++ src/components/templates/seller-introduction/SellerLocation.tsx
@@ -0,0 +1,67 @@
+import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
+import { PiMapPinFill } from "react-icons/pi";
+import { SiGooglemaps } from "react-icons/si";
+import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
+import RegisterTitle from "./RegisterTitle";
+import Button from "@/components/ui/Button";
+
+function LocationTracker({ setPosition }) {
+  const map = useMapEvents({
+    moveend() {
+      const center = map.getCenter();
+      setPosition([center.lat, center.lng]);
+    },
+  });
+
+  return null;
+}
+
+const SellerLocation = () => {
+  const [position, setPosition] = useState<[number, number]>([35.7, 51.39]);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return (
+      <div className="flex h-96 items-center justify-center text-gray-600">
+        در حال بارگذاری نقشه...
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <RegisterTitle content="لوکیشن خود را در نقشه زیر انتخاب کنید" />
+      <div className="max-w-4xl mx-auto p-5 mt-8 md:mt-10">
+        <div className="relative h-[400px] border-2 border-gray-300 rounded-lg overflow-hidden">
+          <MapContainer center={position} zoom={13} className="h-full w-full">
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <LocationTracker setPosition={setPosition} />
+          </MapContainer>
+
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-5xl z-[1000] pointer-events-none">
+            <SiGooglemaps className="text-[50px] text-red-600" />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-center gap-x-4">
+        <Button type="submit" className="!w-[200px] text-sm md:text-base">
+          ذخیره لوکیشن
+        </Button>
+        <Button
+          type="reset"
+          className="!w-[200px] !bg-red-500 text-sm md:text-base"
+        >
+          لغو
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default SellerLocation;
