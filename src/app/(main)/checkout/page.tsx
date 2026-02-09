b9a83fa3454ed40203f49f634916e0c34c7601bb
Milad salamian <miladsalami1385@gmail.com>
2026-02-10 01:15:14 +0330

    add checkout basket page

---
+++ src/app/(main)/checkout/page.tsx
@@ -0,0 +1,19 @@
+import Breadcrumb from "@/components/modules/main/Breadcrumb";
+import Container from "@/components/modules/main/Container";
+import React from "react";
+
+function page() {
+  return (
+    <Container>
+      <Breadcrumb
+        links={[
+          { id: 1, href: "/", name: "صفحه اصلی" },
+          { id: 2, href: "/cart", name: "سبد خرید" },
+          { id: 3, href: "/checkout", name: "ثبت سفارش" },
+        ]}
+      />
+    </Container>
+  );
+}
+
+export default page;
