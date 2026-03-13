b4f13503f4cade95c81ec720bc8d33abf89e2eef
Milad salamian <miladsalami1385@gmail.com>
2026-03-14 01:21:16 +0330

    add responsive layout in checkout component

--- src/components/templates/checkout/Checkout.tsx
+++ src/components/templates/checkout/Checkout.tsx
@@ -1,24 +1,24 @@
 function Checkout() {
   return (
     <div className="col-span-12 md:col-span-3">
-      <div className="bg-white rounded-3xl p-8">
+      <div className="bg-white rounded-3xl p-5 md:p-8">
         <div className="flex flex-col font-Lalezar  text-base">
-          <div className="flex items-center justify-between p-4">
+          <div className="flex items-center justify-between p-4 text-sm md:text-base">
             <span>مبلغ کل (2) : </span>
-            <span>300.000 تومان</span>
+            <span className="text-zinc-700">300.000 تومان</span>
           </div>
-          <div className="flex items-center justify-between p-4 bg-yellow-100 rounded-lg">
+          <div className="flex items-center justify-between p-4 text-sm md:text-base bg-yellow-100 rounded-lg">
             <span>ارسال عادی : </span>
             {/* <span>140.000</span> */}
             <span className="text-sm text-zinc-800">مشخص نشده</span>
           </div>
-          <div className="flex items-center justify-between p-4">
+          <div className="flex items-center justify-between p-4 text-sm md:text-base">
             <span>سود شما : </span>
-            <span>100.000 تومان</span>
+            <span className="text-zinc-700">100.000 تومان</span>
           </div>
-          <div className="flex items-center justify-between p-4 bg-yellow-100 rounded-lg mb-4">
+          <div className="flex items-center justify-between p-4 text-sm md:text-base bg-yellow-100 rounded-lg mb-4">
             <span>مبلغ نهایی:</span>
-            <span>200.000 تومان</span>
+            <span className="text-zinc-700">200.000 تومان</span>
           </div>
           <button className="flex items-center justify-center py-2 rounded-md bg-stone-800 hover:bg-stone-900 text-white">
             پرداخت
