0bdd9966ed245ac4a37700f7cbfa84b6347a5d5d
Milad salamian <miladsalami1385@gmail.com>
2026-03-15 18:59:15 +0330

    add  choose time in main component

--- src/components/templates/checkout/ChooseTime.tsx
+++ src/components/templates/checkout/ChooseTime.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Title from "./Title";
+import Time from "./Time";
+import { deliverTimes } from "@/constants/data";
 
 function ChooseTime() {
-  return <div className="mt-5">
-    <Title title="انتخاب زمان ارسال"/>
-  </div>;
+  
+  return (
+    <div className="mt-5">
+      <Title title="انتخاب زمان ارسال" />
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 pt-6 gap-5">
+        {deliverTimes.map(time =>(
+          <Time {...time} key={time.id}/>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ChooseTime;
