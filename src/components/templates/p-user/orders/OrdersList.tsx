97ec1a9e1f876c37e742be9a277db6329957fbfb
Milad salamian <miladsalami1385@gmail.com>
2026-03-16 16:40:38 +0330

    add orders list component in p-user

---
+++ src/components/templates/p-user/orders/OrdersList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import OrderItem from "../../checkout/OrderItem";
+import Button from "@/components/ui/Button";
+import Link from "next/link";
+
+function OrdersList() {
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5 p-5 pb-0">
+        <OrderItem />
+        <OrderItem />
+        <OrderItem />
+        <OrderItem />
+      </div>
+      <div className="flex items-center gap-x-4 px-5 mt-8">
+        <Button className="!w-[150px]">مشاهده فاکتور</Button>
+        <Link href="/p-user/orders/">
+          <Button className="!w-[150px] !bg-red-500">بازگشت</Button>
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default OrdersList;
