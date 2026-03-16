3cfedab8b403733b64391acab75c61f37c5fc7d5
Milad salamian <miladsalami1385@gmail.com>
2026-03-16 16:41:22 +0330

    add new feature in title component

--- src/components/modules/p-user/Title.tsx
+++ src/components/modules/p-user/Title.tsx
@@ -1,11 +1,21 @@
+"use client";
 import { IUserPanelTitle } from "@/libs/types";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { HiArrowLeft } from "react-icons/hi";
 
-function Title({ content , children }: IUserPanelTitle) {
+function Title({ content, isBack, children }: IUserPanelTitle) {
+  const router = useRouter();
   return (
     <div className="p-3 bg-stone-200 rounded-xl my-4 flex items-center justify-between">
       <h2 className="font-Lalezar">{content}</h2>
       {children}
+      {isBack && (
+        <HiArrowLeft
+          className="md:cursor-pointer"
+          onClick={() => router.back()}
+        />
+      )}
     </div>
   );
 }
