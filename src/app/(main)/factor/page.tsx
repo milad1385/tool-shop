507aa1419c2a9e09fab25176cb757935579f5630
Milad salamian <miladsalami1385@gmail.com>
2026-03-17 16:14:13 +0330

    add factor page

---
+++ src/app/(main)/factor/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import FactorDetails from "@/components/templates/p-user/orders/FactorDetails";
+function Factor() {
+  return <FactorDetails />;
+}
+
+export default Factor;
