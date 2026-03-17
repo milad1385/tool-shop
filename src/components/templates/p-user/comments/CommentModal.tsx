cfcc8d4087dc9e3770a23588b8e0849df7af73e4
Milad salamian <miladsalami1385@gmail.com>
2026-03-17 17:01:43 +0330

    add comment modal component

---
+++ src/components/templates/p-user/comments/CommentModal.tsx
@@ -0,0 +1,30 @@
+import Button from "@/components/ui/Button";
+import React from "react";
+import { FaXmark } from "react-icons/fa6";
+
+function CommentModal({ onClose }: { onClose?: any }) {
+  return (
+    <div className="w-[340px] md:w-[500px] rounded-md bg-white px-4 py-5">
+      <div className="flex items-center justify-between border-b-2 border-b-gray-200 pb-4">
+        <h3 className="font-bold text-base md:text-[17px]">کامنت محمد اکبری</h3>
+        <FaXmark
+          onClick={() => onClose()}
+          className="text-xl text-zinc-500 md:cursor-pointer"
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        <p className="text-sm md:text-base/[32px] p-2 text-justify">
+          سلام ، به نظر من کالا بسیار مفیدی بود و همچنین فروشنده به سرعت و
+          کمترین قیمت نسبت به سایر فروشنده ها این کالا بسیار خوب را عرضه کردند ،
+          ممنون از سایت ترازو.
+        </p>
+
+        <Button className="!w-[200px] !rounded-lg" onClick={() => onClose()}>
+          مشاهده کردم
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default CommentModal;
