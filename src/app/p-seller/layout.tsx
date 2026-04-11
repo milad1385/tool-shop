56865a9bf9111035187c4c16a38d6a13cfd8ab8a
Milad salamian <miladsalami1385@gmail.com>
2026-04-12 00:46:51 +0330

    add p-seller layout file

---
+++ src/app/p-seller/layout.tsx
@@ -0,0 +1,30 @@
+import Sidebar from "@/components/modules/p-admin/Sidebar";
+import Topbar from "@/components/modules/p-admin/Topbar";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | پنل فروشنده",
+    default: "پنل فروشنده  ترازو",
+  },
+  description:
+    "پنل فروشنده ترازو برای مدیریت محتوا های سایت ترازو توسط فروشنده های وب سایت میباشد",
+  icons: {
+    icon: "/images/tool.png",
+  },
+};
+
+function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="grid grid-cols-12 h-screen">
+      <Sidebar />
+      <div className="col-span-12 lg:col-span-10">
+        <Topbar />
+        <div className="p-5">{children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default layout;
