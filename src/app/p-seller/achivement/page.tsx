1511a9aca6641d25b08eb35a6c27996c791334f3
Milad salamian <miladsalami1385@gmail.com>
2026-04-13 02:09:54 +0330

    add top bar in achivement p-seller page

--- src/app/p-seller/achivement/page.tsx
+++ src/app/p-seller/achivement/page.tsx
@@ -1,3 +1,6 @@
+import Container from "@/components/modules/p-admin/Container";
+import Filters from "@/components/modules/p-admin/Filters";
+import PageTitle from "@/components/modules/p-admin/PageTitle";
 import { Metadata } from "next";
 import React from "react";
 
@@ -6,7 +9,22 @@ export const metadata: Metadata = {
 };
 
 function page() {
-  return <div>page</div>;
+  return (
+    <Container>
+      <div className="flex items-center justify-between">
+        <PageTitle content="میزان درآمد" />
+        <Filters
+          filterField="last"
+          options={[
+            { label: "7 روز گذشته", slug: "7" },
+            { label: "30 روز گذشته", slug: "30" },
+            { label: "90 روز گذشته", slug: "90" },
+            { label: "120 روز گذشته", slug: "120" },
+          ]}
+        />
+      </div>
+    </Container>
+  );
 }
 
 export default page;
