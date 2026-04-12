d3c30f0bb01f075aab8783956308503e1568279b
Milad salamian <miladsalami1385@gmail.com>
2026-04-13 01:28:53 +0330

    add filter component in p-seller index page

--- src/app/p-seller/page.tsx
+++ src/app/p-seller/page.tsx
@@ -1,10 +1,20 @@
 import Container from "@/components/modules/p-admin/Container";
+import Filters from "@/components/modules/p-admin/Filters";
 import Stats from "@/components/templates/p-seller/Stats";
 import React from "react";
 
 function page() {
   return (
     <Container>
+      <Filters
+        filterField="last"
+        options={[
+          { label: "7 روز گذشته", slug: "7" },
+          { label: "30 روز گذشته", slug: "30" },
+          { label: "90 روز گذشته", slug: "90" },
+          { label: "120 روز گذشته", slug: "120" },
+        ]}
+      />
       <Stats />
     </Container>
   );
