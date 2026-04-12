719f27dfabdd0a9d94cb719030bee131a915ecf1
Milad salamian <miladsalami1385@gmail.com>
2026-04-13 01:38:01 +0330

    use sales chart component in p-seller index page

--- src/app/p-seller/page.tsx
+++ src/app/p-seller/page.tsx
@@ -1,7 +1,10 @@
 import Container from "@/components/modules/p-admin/Container";
 import Filters from "@/components/modules/p-admin/Filters";
+import SalesChart from "@/components/modules/p-admin/SalesChart";
+import DurationChart from "@/components/templates/p-admin/DurationChart";
+import RecentProducts from "@/components/templates/p-admin/RecentProducts";
 import Stats from "@/components/templates/p-seller/Stats";
-import React from "react";
+import { durationChartData, salesChartData } from "@/constants/data";
 
 function page() {
   return (
@@ -16,6 +19,13 @@ function page() {
         ]}
       />
       <Stats />
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-5">
+        <DurationChart title="میزان فروش کالا" data={durationChartData} />
+        <RecentProducts />
+      </div>
+
+      <SalesChart data={salesChartData} />
     </Container>
   );
 }
