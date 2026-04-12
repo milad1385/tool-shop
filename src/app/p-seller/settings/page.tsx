3e1a6648e83c48da89a0cfb9f578d257869a944f
Milad salamian <miladsalami1385@gmail.com>
2026-04-13 02:10:23 +0330

    add topbar in settings page p-seller

--- src/app/p-seller/settings/page.tsx
+++ src/app/p-seller/settings/page.tsx
@@ -1,16 +1,18 @@
-import { Metadata } from 'next';
-import React from 'react'
-
+import Container from "@/components/modules/p-admin/Container";
+import PageTitle from "@/components/modules/p-admin/PageTitle";
+import { Metadata } from "next";
+import React from "react";
 
 export const metadata: Metadata = {
-  title: "تنظیمات فروشگاه"
+  title: "تنظیمات فروشگاه",
 };
-
 
 function page() {
   return (
-    <div>page</div>
-  )
+    <Container>
+      <PageTitle content="تنظیمات فروشگاه" />
+    </Container>
+  );
 }
 
-export default page
+export default page;
