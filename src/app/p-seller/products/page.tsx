99fdd24a5ec4123c74b933c6c50974e3054b8d03
Milad salamian <miladsalami1385@gmail.com>
2026-04-23 01:21:05 +0330

    use request product list component in product page in p-seller

--- src/app/p-seller/products/page.tsx
+++ src/app/p-seller/products/page.tsx
@@ -2,7 +2,8 @@ import Container from '@/components/modules/p-admin/Container';
 import PageTitle from '@/components/modules/p-admin/PageTitle';
 import TableOperation from '@/components/modules/p-admin/TableOpration';
 import RequestNewProduct from '@/components/templates/p-seller/RequestNewProduct';
-import { productFilterOptions } from '@/constants/data';
+import RequestProductList from '@/components/templates/p-seller/RequestProductList';
+import { requestProductFilterOptions } from '@/constants/data';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -15,8 +16,8 @@ function page() {
     <Container>
       <PageTitle content="درخواست محصول جدید" />
       <RequestNewProduct/>
-      <TableOperation pageTitle="لیست محصولات" options={productFilterOptions} />
-      
+      <TableOperation pageTitle="لیست محصولات" options={requestProductFilterOptions} />
+      <RequestProductList/>
     </Container>
   )
 }
