ee8b2efd56a502417adf4ae259e7f8ff3896b3ec
Milad salamian <miladsalami1385@gmail.com>
2026-07-15 20:58:15 +0330

    use mobile menu sidebar in top bar

--- src/components/modules/p-admin/Topbar.tsx
+++ src/components/modules/p-admin/Topbar.tsx
@@ -1,23 +1,27 @@
 import Image from "next/image";
 import Logout from "@/components/templates/p-admin/Logout";
 import { ITopbar } from "@/libs/types";
+import SidebarMenu from "./SidebarMenu";
 
 function Topbar({ role }: ITopbar) {
   return (
     <div className="bg-white sticky top-0 z-40 p-4 flex items-center justify-between">
       <div className="flex items-center gap-x-3">
-        <Image
-          src="/images/user.jpg"
-          width={1920}
-          height={1080}
-          alt="user-profile"
-          className="w-[50px] h-[50px] rounded-full"
-        />
-        <div className="flex flex-col gap-y-1">
-          <span className="font-Lalezar">میلاد سلامیان</span>
-          <span className="text-xs font-IranMedium text-gray-700">
-            {role === "seller" ? "فروشنده" : "ادمین کل"}
-          </span>
+        <SidebarMenu />
+        <div className="flex items-center gap-x-3">
+          <Image
+            src="/images/user.jpg"
+            width={1920}
+            height={1080}
+            alt="user-profile"
+            className="w-[50px] h-[50px] rounded-full"
+          />
+          <div className="flex flex-col gap-y-1">
+            <span className="font-Lalezar">میلاد سلامیان</span>
+            <span className="text-xs font-IranMedium text-gray-700">
+              {role === "seller" ? "فروشنده" : "ادمین کل"}
+            </span>
+          </div>
         </div>
       </div>
       <div>
