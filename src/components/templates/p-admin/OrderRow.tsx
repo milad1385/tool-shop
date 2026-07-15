7562c4f8bbda7cb0ad29019f48214b01e48472ba
Milad salamian <miladsalami1385@gmail.com>
2026-07-15 21:50:53 +0330

    fix order row style bug

--- src/components/templates/p-admin/OrderRow.tsx
+++ src/components/templates/p-admin/OrderRow.tsx
@@ -3,22 +3,21 @@ import { FaEye, FaTrash } from "react-icons/fa";
 
 function OrderRow() {
   return (
-    <tr>
-      <td>1</td>
-
-      <td>میلاد سلامیان</td>
-      <td>1404/03/18</td>
-      <td>25,000</td>
-      <td>کرج</td>
-      <td>
-        <div className="bg-green-600 text-white rounded-md text-xs py-2 w-[40px] mx-auto">
-            تحویل
+    <tr className="border-b border-gray-100">
+      <td className="px-2 py-2 text-center">1</td>
+      <td className="px-2 py-2 text-center">میلاد سلامیان</td>
+      <td className="px-2 py-2 text-center">1404/03/18</td>
+      <td className="px-2 py-2 text-center">25,000</td>
+      <td className="px-2 py-2 text-center">کرج</td>
+      <td className="px-2 py-2 text-center">
+        <div className="bg-green-600 text-white rounded-md text-sm py-2 px-2 w-[50px] mx-auto">
+          تحویل
         </div>
       </td>
-      <td>
+      <td className="px-2 py-2 text-center">
         <div className="flex items-center gap-x-3 justify-center">
-          <FaTrash className="-mt-1 text-red-700" />
-          <FaEye className="-mt-1 text-yellow-500 md:cursor-pointer" />
+          <FaTrash className="text-red-700 text-base cursor-pointer" />
+          <FaEye className="text-yellow-500 text-base cursor-pointer" />
         </div>
       </td>
     </tr>
