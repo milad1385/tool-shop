2fe3300ea1fa7622b4fa5504ba79a4fe93a35609
Milad salamian <miladsalami1385@gmail.com>
2026-07-15 21:50:45 +0330

    fix product row style bug

--- src/components/templates/p-admin/ProductRow.tsx
+++ src/components/templates/p-admin/ProductRow.tsx
@@ -4,24 +4,24 @@ import { FaEye, FaTrash } from "react-icons/fa";
 
 function ProductRow() {
   return (
-    <tr>
-      <td>1</td>
-      <td>
+    <tr className="border-b border-gray-100">
+      <td className="px-2 py-2 text-center">1</td>
+      <td className="px-2 py-2 text-center">
         <Image
           src="/images/product-1.jpg"
-          alt="user.jpg"
+          alt="product.jpg"
           width={1920}
           height={1080}
-          className="w-8 md:w-16 h-8 md:h-16 rounded-full mx-auto"
+          className="w-12 md:w-12 h-12 md:h-12 rounded-full mx-auto object-cover"
         />
       </td>
-      <td>دریل شارژی</td>
-      <td>1404/04/01</td>
-      <td>25,000</td>
-      <td>
+      <td className="px-2 py-2 text-center">دریل شارژی</td>
+      <td className="px-2 py-2 text-center">1404/04/01</td>
+      <td className="px-2 py-2 text-center">25,000</td>
+      <td className="px-2 py-2 text-center">
         <div className="flex items-center gap-x-3 justify-center">
-          <FaTrash className="-mt-1 text-red-700" />
-          <FaEye className="-mt-1 text-yellow-500 md:cursor-pointer" />
+          <FaTrash className="text-red-700 text-base  cursor-pointer hover:text-red-900 transition-colors" />
+          <FaEye className="text-yellow-500 text-base  cursor-pointer hover:text-yellow-700 transition-colors" />
         </div>
       </td>
     </tr>
