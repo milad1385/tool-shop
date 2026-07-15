53a8f3423db8951bd10fc8914033f647e19cff41
Milad salamian <miladsalami1385@gmail.com>
2026-07-15 21:41:03 +0330

    fix title style bug

--- src/components/modules/p-admin/Title.tsx
+++ src/components/modules/p-admin/Title.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Title({ content }: { content: string }) {
   const text = content.split(" ");
   return (
-    <h2 className="text-lg md:text-xl lg:text-2xl pb-2 font-Lalezar text-zinc-700">
+    <h2 className="text-xl lg:text-2xl pb-2 font-Lalezar text-zinc-700">
       <span className="text-yellow-500">{text[0]}</span> {text[1]} {text?.[2]}
     </h2>
   );
