4b5b898349e153904cb9ddd7e9589656361997b3
Milad salamian <miladsalami1385@gmail.com>
2026-07-16 17:14:25 +0330

    fix filters componnet style bug

--- src/components/modules/p-admin/Filters.tsx
+++ src/components/modules/p-admin/Filters.tsx
@@ -26,13 +26,13 @@ function Filter({ filterField, options }: TFilter) {
   };
 
   return (
-    <div className="flex w-full md:inline-flex items-center gap-x-0.5 md:gap-x-2 font-Dana bg-white p-1 child:transition-all child:cursor-pointer text-xs md:text-sm rounded-md mt-5 lg:mt-0">
+    <div className="flex w-full md:w-auto  md:inline-flex items-center gap-x-0.5 md:gap-x-2 font-Dana bg-white p-1 child:transition-all child:cursor-pointer text-xs md:text-sm rounded-md mt-5 lg:mt-0">
       {options.map((option, index) => (
         <div
           key={index}
           className={`text-zinc-800 flex items-center justify-center flex-1 md:flex-initial hover:bg-yellow-500 ${
             option.slug === paramValue ? "bg-yellow-500 !text-white" : ""
-          } hover:text-white py-1 px-1 md:px-2 rounded-md whitespace-nowrap`}
+          } hover:text-white py-2 md:py-1.5 px-1 md:px-2 rounded-md whitespace-nowrap`}
           onClick={() => handleFilterParm(option.slug)}
         >
           {option.label}
