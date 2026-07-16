dc40c0ddd94b4d712885ad67265631e65db8e582
Milad salamian <miladsalami1385@gmail.com>
2026-07-16 18:16:32 +0330

    add responsive design in order box item

--- src/components/templates/p-admin/OrderBoxItem.tsx
+++ src/components/templates/p-admin/OrderBoxItem.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 function OrderBoxItem() {
   return (
-    <div className="flex gap-x-8 items-center  py-3">
+    <div className="flex gap-x-8 items-center  py-3 text-sm md:text-base">
       <Link href="/products/1">
         <Image
           src="/images/product-3.jpg"
@@ -14,10 +14,16 @@ function OrderBoxItem() {
           className="w-24"
         />
       </Link>
-      <Link href="/products/1">نام : دریل شارژی</Link>
-      <p>نام فروشنده : فروشنده تست</p>
-      <p>تعداد : 1</p>
-      <p>قیمت : 350,000 تومان</p>
+      <div className="block  md:hidden space-y-3 text-zinc-700">
+        <Link href="/products/1">نام : دریل شارژی</Link>
+        <p>نام فروشنده : فروشنده تست</p>
+        <p>تعداد : 1</p>
+        <p>قیمت : 350,000 تومان</p>
+      </div>
+      <Link href="/products/1" className="hidden md:block">نام : دریل شارژی</Link>
+      <p className="hidden md:block">نام فروشنده : فروشنده تست</p>
+      <p className="hidden md:block">تعداد : 1</p>
+      <p className="hidden md:block">قیمت : 350,000 تومان</p>
     </div>
   );
 }
