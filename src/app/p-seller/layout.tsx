e43bfd3a01adbb035778038471c6497ae9c5b19b
Milad salamian <miladsalami1385@gmail.com>
2026-07-17 23:49:17 +0330

    fix p-seller layout style bug

--- src/app/p-seller/layout.tsx
+++ src/app/p-seller/layout.tsx
@@ -21,7 +21,7 @@ function layout({ children }: { children: React.ReactNode }) {
       <Sidebar title="پنل مدیریت فروشنده" />
       <div className="col-span-12 lg:col-span-10">
         <Topbar role="seller" />
-        <div className="md:p-5">{children}</div>
+        <div className="md:p-5 pb-16">{children}</div>
       </div>
     </div>
   );
