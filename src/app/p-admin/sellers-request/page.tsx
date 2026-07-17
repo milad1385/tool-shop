cab0fa69dbcf81088b6c3b623c2f353c74a38480
Milad salamian <miladsalami1385@gmail.com>
2026-07-17 12:50:04 +0330

    add sellers-requests p-admin components folder

--- src/app/p-admin/sellers-request/page.tsx
+++ src/app/p-admin/sellers-request/page.tsx
@@ -1,6 +1,6 @@
 import Container from "@/components/modules/p-admin/Container";
 import TableOperation from "@/components/modules/p-admin/TableOpration";
-import SellerRequestList from "@/components/templates/p-admin/SellerRequestList";
+import SellerRequestList from "@/components/templates/p-admin/sellers-requests/SellerRequestList";
 import { statusFilterOptions } from "@/constants/data";
 
 function page() {
