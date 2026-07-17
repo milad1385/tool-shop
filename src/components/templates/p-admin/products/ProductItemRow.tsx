13744b92227b5aa71439243cdd34abc706899f13
Milad salamian <miladsalami1385@gmail.com>
2026-07-17 12:32:27 +0330

    add product item row component

---
+++ src/components/templates/p-admin/products/ProductItemRow.tsx
@@ -0,0 +1,36 @@
+import Table from "@/components/modules/p-admin/Table";
+import Image from "next/image";
+import React from "react";
+import { FaEye, FaTrash } from "react-icons/fa";
+import { FaPencil } from "react-icons/fa6";
+
+function ProductItemRow() {
+  return (
+    <Table.Row>
+      <td>1</td>
+      <td className="!p-0 md:!p-5">
+        <Image
+          src="/images/product-7.jpg"
+          className="w-20 rounded-md mx-auto my-2 md:my-0"
+          alt=""
+          width={1920}
+          height={1080}
+        />
+      </td>
+      <td>دریل رونیکس</td>
+      <td>Ronix</td>
+      <td>دریل</td>
+      <td>رونیکس ، ابزار</td>
+      <td>1404/04/25</td>
+      <td>
+        <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
+          <FaPencil className="text-yellow-500 text-base md:text-xl" />
+          <FaTrash className="text-red-600 text-base md:text-xl" />
+          <FaEye className="text-sky-500 text-base md:text-xl" />
+        </div>
+      </td>
+    </Table.Row>
+  );
+}
+
+export default ProductItemRow;
