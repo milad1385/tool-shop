61619b3a14d7a82d48273edd9e3dccad8c866c05
Milad salamian <miladsalami1385@gmail.com>
2026-07-17 12:16:57 +0330

    fix table operation style bug

--- src/components/modules/p-admin/TableOpration.tsx
+++ src/components/modules/p-admin/TableOpration.tsx
@@ -5,7 +5,7 @@ import Search from "./Search";
 
 function TableOperation({ pageTitle, options }: ITableOperation) {
   return (
-    <div className="flex flex-col md:flex-row md:items-center justify-between mt-4 mb-8">
+    <div className="flex flex-col md:flex-row md:items-center justify-between mb-8">
       <PageTitle content={pageTitle} />
       <div className="flex flex-col-reverse md:flex-row space-y-6 items-center gap-x-4">
         {options?.length && <Filters filterField="status" options={options} />}
