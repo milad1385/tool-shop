ef1ef20c4b70f3574f7003c5a93f4b3deb602395
Milad salamian <miladsalami1385@gmail.com>
2026-07-17 12:34:19 +0330

    add articles p-admin components folder

--- src/app/p-admin/articles/page.tsx
+++ src/app/p-admin/articles/page.tsx
@@ -1,9 +1,9 @@
 import Container from "@/components/modules/p-admin/Container";
 import TableOperation from "@/components/modules/p-admin/TableOpration";
-import ArticleList from "@/components/templates/p-admin/ArticleList";
+import ArticleList from "@/components/templates/p-admin/articles/ArticleList";
 import { articleFilter } from "@/constants/data";
 import PageTitle from "../../../components/modules/p-admin/PageTitle";
-import CreateNewArticle from "@/components/templates/p-admin/CreateNewArticle";
+import CreateNewArticle from "@/components/templates/p-admin/articles/CreateNewArticle";
 
 function page() {
   return (
