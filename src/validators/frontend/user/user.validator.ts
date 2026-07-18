5838b73af97c52d8fb45fb92989759b9f563ba5d
Milad salamian <miladsalami1385@gmail.com>
2026-07-18 20:59:49 +0330

    add changePassword validator

--- src/validators/frontend/user/user.validator.ts
+++ src/validators/frontend/user/user.validator.ts
@@ -152,6 +152,26 @@ export const forgotUserPassword = yup.object({
     .max(100, "حداکثر تعداد کاراکتر 100 عدد میباشد"),
 });
 
+export const changePassword = yup.object({
+  password: yup
+    .string()
+    .required("رمز عبور الزامی است")
+    .min(8, "رمز عبور باید حداقل ۸ کاراکتر باشد")
+    .max(100, "رمز عبور نمی‌تواند بیشتر از ۱۰۰ کاراکتر باشد")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "رمز عبور باید شامل حروف بزرگ، کوچک، عدد و کاراکتر خاص باشد",
+    ),
+
+  confirmPassword: yup
+    .string()
+    .required("تکرار رمز عبور الزامی است")
+    .oneOf([yup.ref("password")], "رمز عبور و تکرار آن مطابقت ندارند")
+    .min(8, "تکرار رمز عبور باید حداقل ۸ کاراکتر باشد")
+    .max(100, "تکرار رمز عبور نمی‌تواند بیشتر از ۱۰۰ کاراکتر باشد"),
+});
+
 export type sellerAddressInfoType = yup.InferType<typeof sellerAddressInfo>;
 export type UserAddressType = yup.InferType<typeof userAddress>;
 export type forgotUserPasswordType = yup.InferType<typeof forgotUserPassword>;
+export type changePasswordType = yup.InferType<typeof changePassword>;
