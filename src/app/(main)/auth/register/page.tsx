886f116fe39af30323e8810f6b97f11c536cf033
Milad salamian <miladsalami1385@gmail.com>
2026-07-18 21:20:41 +0330

    fix register page bug

--- src/app/(main)/auth/register/page.tsx
+++ src/app/(main)/auth/register/page.tsx
@@ -1,8 +1,6 @@
 import RegisterForm from "@/components/templates/auth/RegisterForm";
 import { Metadata } from "next";
 import Image from "next/image";
-import Link from "next/link";
-import React from "react";
 
 export const metadata: Metadata = {
   title: "ثبت نام کنید",
