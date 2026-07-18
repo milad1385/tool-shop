61847fc00ca7c97d6eb3b1d322b2559092f8645b
Milad salamian <miladsalami1385@gmail.com>
2026-07-18 20:23:33 +0330

    fix logout  component style bug

--- src/components/modules/p-admin/Logout.tsx
+++ src/components/modules/p-admin/Logout.tsx
@@ -4,7 +4,7 @@ import { HiMiniArrowRightEndOnRectangle } from "react-icons/hi2";
 function Logout() {
   return (
     <div
-      className={`flex w-[215px] items-center gap-x-2 text-[15px] cursor-pointer  md:text-base p-3 md:py-3.5 md:px-4 hover:bg-stone-100 rounded-md duration-300`}
+      className={`flex items-center gap-x-2 text-[15px] cursor-pointer  md:text-base p-3 md:py-3.5 md:px-4 hover:bg-stone-100 rounded-md duration-300`}
     >
       <HiMiniArrowRightEndOnRectangle className="text-2xl text-zinc-700" />
       <span className="mr-1">خروج</span>
