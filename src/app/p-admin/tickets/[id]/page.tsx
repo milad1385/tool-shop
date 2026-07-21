e3aba5186576b3ea3353e9378c610315ef834e2d
Milad salamian <miladsalami1385@gmail.com>
2026-07-21 12:50:31 +0330

    use detail ticket component

--- src/app/p-admin/tickets/[id]/page.tsx
+++ src/app/p-admin/tickets/[id]/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Container from "@/components/modules/p-user/Container";
+import Title from "@/components/modules/p-user/Title";
+import AnswerList from "@/components/templates/p-user/tickets/AnswerList";
+import React from "react";
 
 function page() {
   return (
-    <div>page</div>
-  )
+    <Container>
+      <Title content={`درخواست سرمایه گذاری در سایت`} />
+      <AnswerList/>
+    </Container>
+  );
 }
 
-export default page
+export default page;
