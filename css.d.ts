d1b2a8f18ea80ce23d1f5f6e006c191c0f090ee8
Milad salamian <miladsalami1385@gmail.com>
2026-07-29 16:36:42 +0330

    add css types

--- css.d.ts
+++ css.d.ts
@@ -2,3 +2,59 @@ declare module "*.css" {
   const content: { [className: string]: string };
   export default content;
 }
+
+// swiper.d.ts یا globals.d.ts
+declare module 'swiper/css' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/navigation' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/pagination' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/scrollbar' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/effect-fade' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/effect-coverflow' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/effect-flip' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/effect-cube' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/thumbs' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/free-mode' {
+  const content: any;
+  export default content;
+}
+
+declare module 'swiper/css/autoplay' {
+  const content: any;
+  export default content;
+}
