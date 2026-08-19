f3c4a4800b1507da5894a1fb2cc7b2fb765bcdd1
Milad salamian <miladsalami1385@gmail.com>
2026-08-19 17:31:37 +0330

    init toaster popup in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "@/components/modules/main/Footer";
 import NextTopLoader from "nextjs-toploader";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
   title: {
@@ -31,6 +32,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Toaster />
       </body>
     </html>
   );
