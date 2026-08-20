a75761c72b8610dd3fcce4d0c37c740a575a2729
Milad salamian <miladsalami1385@gmail.com>
2026-08-20 18:16:56 +0330

    add proxy file

---
+++ src/proxy.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const accessToken = request.cookies.get("accessToken");
+  console.log(accessToken);
+
+  if (pathname.startsWith("/auth")) {
+    if (accessToken) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+}
+
+export const config = {
+  matcher: ["/auth/:path*"],
+};
