340c5a2a52991f31c87b0cab020c7c3d775ca3fe
Milad salamian <miladsalami1385@gmail.com>
2026-08-22 11:11:41 +0330

    add route protection for p-admin in proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import jwt from "jsonwebtoken";
+import { IVerifyUser, UserRoleEnums } from "./libs/types";
+import { verifyToken } from "./utils/helper";
 export function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   const accessToken = request.cookies.get("accessToken");
@@ -15,8 +17,22 @@ export function proxy(request: NextRequest) {
       return NextResponse.redirect(new URL("/", request.url));
     }
   }
+
+  if (pathname.startsWith("/p-admin")) {
+    if (!accessToken) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+    const user = verifyToken(accessToken.value);
+
+    const isAdmin = user.roles.some((role) =>
+      [UserRoleEnums.SUPER_ADMIN, UserRoleEnums.ADMIN].includes(role),
+    );
+    if (!isAdmin) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
 }
 
 export const config = {
-  matcher: ["/auth/:path*", "/p-user/:path*"],
+  matcher: ["/auth/:path*", "/p-user/:path*", "/p-admin/:path*"],
 };
