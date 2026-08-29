1e07401b8045a40613c11eada14ca8c96677545f
Milad salamian <miladsalami1385@gmail.com>
2026-08-29 15:48:28 +0330

    enhance speed of categories page

--- src/app/p-admin/categories/page.tsx
+++ src/app/p-admin/categories/page.tsx
@@ -4,7 +4,10 @@ import TableOperation from "@/components/modules/p-admin/TableOpration";
 import CategoryList from "@/components/templates/p-admin/categories/CategoryList";
 import CreateCategory from "@/components/templates/p-admin/categories/CreateCategory";
 import { IPage } from "@/libs/types";
-import { getCategoriesWithPagination } from "@/services/categories.service";
+import {
+  getAllCategories,
+  getCategoriesWithPagination,
+} from "@/services/categories.service";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -16,17 +19,24 @@ async function page({ searchParams }: IPage) {
   const page = Number(params.page) || 1;
   const limit = Number(params.limit) || 10;
   const search = params.q;
-  const { data, pagination } = await getCategoriesWithPagination({
-    page,
-    limit,
-    search,
-  });
+  const [allCategories, categories] = await Promise.all([
+    getCategoriesWithPagination({
+      page,
+      limit,
+      search,
+    }),
+    getAllCategories(),
+  ]);
+
   return (
     <Container>
       <PageTitle content="ایجاد دسته بندی" />
-      <CreateCategory />
+      <CreateCategory categories={categories} />
       <TableOperation pageTitle="لیست دسته بندی ها" />
-      <CategoryList data={data} pagination={pagination} />
+      <CategoryList
+        data={allCategories.data}
+        pagination={allCategories.pagination}
+      />
     </Container>
   );
 }
