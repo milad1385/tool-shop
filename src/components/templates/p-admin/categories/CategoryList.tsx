d9a8c754385de357dad08960bc27efcc3a6ac5a6
Milad salamian <miladsalami1385@gmail.com>
2026-08-29 13:57:58 +0330

    generate CategoryList data

--- src/components/templates/p-admin/categories/CategoryList.tsx
+++ src/components/templates/p-admin/categories/CategoryList.tsx
@@ -1,149 +1,26 @@
 import Pagination from "@/components/modules/p-admin/Pagination";
 import Table from "@/components/modules/p-admin/Table";
-import Image from "next/image";
-import React from "react";
-import { FaEye, FaTrash } from "react-icons/fa";
-import { FaPencil } from "react-icons/fa6";
+import { ICategoryList } from "@/libs/types";
+import CategoryRow from "./CategoryRow";
+import { categoryTableHeader } from "@/constants/data";
 
-function CategoryList() {
+function CategoryList({ data, pagination }: ICategoryList) {
   return (
     <div className="md:section-box">
       <div className="admin-table mt-5 overflow-hidden  rounded-md">
         <Table>
           <Table.Header>
-            <th>شماره</th>
-            <th>عکس</th>
-            <th>عنوان</th>
-            <th>لینک</th>
-            <th>پرنت</th>
-            <th>تگ ها</th>
-            <th>تاریخ</th>
-            <th>عملیات</th>
+            {categoryTableHeader.map((header, index) => (
+              <th key={index + 1}>{header}</th>
+            ))}
           </Table.Header>
           <Table.Body>
-            <Table.Row>
-              <td>1</td>
-              <td className="!p-0 md:!p-5">
-                <Image
-                  src="/images/ca-2.png"
-                  className="w-12 md:w-16 rounded-md mx-auto my-2 md:my-0"
-                  alt=""
-                  width={1920}
-                  height={1080}
-                />
-              </td>
-              <td>دریل رونیکس</td>
-              <td>Ronix</td>
-              <td>دریل</td>
-              <td>رونیکس ، ابزار</td>
-              <td>1404/04/25</td>
-              <td>
-                <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
-                  <FaPencil className="text-yellow-500 text-base md:text-xl" />
-                  <FaTrash className="text-red-600 text-base md:text-xl" />
-                  <FaEye className="text-sky-500 text-base md:text-xl" />
-                </div>
-              </td>
-            </Table.Row>
-            <Table.Row>
-              <td>2</td>
-              <td className="!p-0 md:!p-5">
-                <Image
-                  src="/images/ca-1.png"
-                  className="w-12 md:w-16 rounded-md mx-auto my-2 md:my-0"
-                  alt=""
-                  width={1920}
-                  height={1080}
-                />
-              </td>
-              <td>دریل رونیکس</td>
-              <td>Ronix</td>
-              <td>دریل</td>
-              <td>رونیکس ، ابزار</td>
-              <td>1404/04/25</td>
-              <td>
-                <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
-                  <FaPencil className="text-yellow-500 text-base md:text-xl" />
-                  <FaTrash className="text-red-600 text-base md:text-xl" />
-                  <FaEye className="text-sky-500 text-base md:text-xl" />
-                </div>
-              </td>
-            </Table.Row>
-            <Table.Row>
-              <td>3</td>
-              <td className="!p-0 md:!p-5">
-                <Image
-                  src="/images/ca-3.png"
-                  className="w-12 md:w-16 rounded-md mx-auto my-2 md:my-0"
-                  alt=""
-                  width={1920}
-                  height={1080}
-                />
-              </td>
-              <td>دریل رونیکس</td>
-              <td>Ronix</td>
-              <td>دریل</td>
-              <td>رونیکس ، ابزار</td>
-              <td>1404/04/25</td>
-              <td>
-                <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
-                  <FaPencil className="text-yellow-500 text-base md:text-xl" />
-                  <FaTrash className="text-red-600 text-base md:text-xl" />
-                  <FaEye className="text-sky-500 text-base md:text-xl" />
-                </div>
-              </td>
-            </Table.Row>
-            <Table.Row>
-              <td>4</td>
-              <td className="!p-0 md:!p-5">
-                <Image
-                  src="/images/ca-4.png"
-                  className="w-12 md:w-16 rounded-md mx-auto my-2 md:my-0"
-                  alt=""
-                  width={1920}
-                  height={1080}
-                />
-              </td>
-              <td>دریل رونیکس</td>
-              <td>Ronix</td>
-              <td>دریل</td>
-              <td>رونیکس ، ابزار</td>
-              <td>1404/04/25</td>
-              <td>
-                <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
-                  <FaPencil className="text-yellow-500 text-base md:text-xl" />
-                  <FaTrash className="text-red-600 text-base md:text-xl" />
-                  <FaEye className="text-sky-500 text-base md:text-xl" />
-                </div>
-              </td>
-            </Table.Row>
-            <Table.Row>
-              <td>5</td>
-              <td className="!p-0 md:!p-5">
-                <Image
-                  src="/images/ca-5.png"
-                  className="w-12 md:w-16 rounded-md mx-auto my-2 md:my-0"
-                  alt=""
-                  width={1920}
-                  height={1080}
-                />
-              </td>
-              <td>دریل رونیکس</td>
-              <td>Ronix</td>
-              <td>دریل</td>
-              <td>رونیکس ، ابزار</td>
-              <td>1404/04/25</td>
-              <td>
-                <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
-                  <FaPencil className="text-yellow-500 text-base md:text-xl" />
-                  <FaTrash className="text-red-600 text-base md:text-xl" />
-                  <FaEye className="text-sky-500 text-base md:text-xl" />
-                </div>
-              </td>
-            </Table.Row>
+            {data.map((category, index) => (
+              <CategoryRow key={category._id} {...category} index={index + 1} />
+            ))}
           </Table.Body>
         </Table>
-        <Pagination count={35} />
+        <Pagination count={pagination.totalItems} />
       </div>
     </div>
   );
