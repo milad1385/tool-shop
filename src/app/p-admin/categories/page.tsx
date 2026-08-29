3c9611a90d6dfabd678f18ba652655976c263c9f
Milad salamian <miladsalami1385@gmail.com>
2026-08-29 13:58:19 +0330

    get and show category data with pagination

--- src/app/p-admin/categories/page.tsx
+++ src/app/p-admin/categories/page.tsx
@@ -3,20 +3,30 @@ import PageTitle from "@/components/modules/p-admin/PageTitle";
 import TableOperation from "@/components/modules/p-admin/TableOpration";
 import CategoryList from "@/components/templates/p-admin/categories/CategoryList";
 import CreateCategory from "@/components/templates/p-admin/categories/CreateCategory";
+import { IPage } from "@/libs/types";
+import { getCategoriesWithPagination } from "@/services/categories.service";
 import { Metadata } from "next";
 
-
 export const metadata: Metadata = {
-  title: "دسته بندی"
+  title: "دسته بندی",
 };
 
-function page() {
+async function page({ searchParams }: IPage) {
+  const params = await searchParams;
+  const page = Number(params.page) || 1;
+  const limit = Number(params.limit) || 10;
+  const search = params.q;
+  const { data, pagination } = await getCategoriesWithPagination({
+    page,
+    limit,
+    search,
+  });
   return (
     <Container>
       <PageTitle content="ایجاد دسته بندی" />
-      <CreateCategory/>
+      <CreateCategory />
       <TableOperation pageTitle="لیست دسته بندی ها" />
-      <CategoryList/>
+      <CategoryList data={data} pagination={pagination} />
     </Container>
   );
 }
