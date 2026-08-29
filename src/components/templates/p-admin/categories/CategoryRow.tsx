947f756d5a2ba7f15dffdf01261e9c1021bc41fe
Milad salamian <miladsalami1385@gmail.com>
2026-08-29 13:55:07 +0330

    generate CategoryRow data

--- src/components/templates/p-admin/categories/CategoryRow.tsx
+++ src/components/templates/p-admin/categories/CategoryRow.tsx
@@ -1,32 +1,46 @@
 import Table from "@/components/modules/p-admin/Table";
+import { ICategory } from "@/libs/types";
+import { formatDate } from "@/utils/helper";
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
 import { FaEye, FaTrash } from "react-icons/fa";
 import { FaPencil } from "react-icons/fa6";
 
-function CategoryRow() {
+function CategoryRow({
+  index,
+  name,
+  href,
+  tags,
+  image,
+  createdAt,
+  parent,
+}: ICategory) {
   return (
     <Table.Row>
-      <td>2</td>
+      <td>{index}</td>
       <td className="!p-0 md:!p-5">
         <Image
-          src="/images/ca-1.png"
+          src={image}
           className="w-12 md:w-16 rounded-md mx-auto my-2 md:my-0"
           alt=""
           width={1920}
           height={1080}
         />
       </td>
-      <td>دریل رونیکس</td>
-      <td>Ronix</td>
-      <td>دریل</td>
-      <td>رونیکس ، ابزار</td>
-      <td>1404/04/25</td>
+      <td>
+        <Link href={`/category/${href}`}>{name}</Link>
+      </td>
+      <td>{href}</td>
+      <td>{parent?.name ?? "---"}</td>
+      <td>{tags.slice(0, 3).join(" ، ")}</td>
+      <td>{formatDate(createdAt)}</td>
       <td>
         <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
           <FaPencil className="text-yellow-500 text-base md:text-xl" />
           <FaTrash className="text-red-600 text-base md:text-xl" />
-          <FaEye className="text-sky-500 text-base md:text-xl" />
+          <Link href={`/category/${href}`}>
+            <FaEye className="text-sky-500 text-base md:text-xl" />
+          </Link>
         </div>
       </td>
     </Table.Row>
