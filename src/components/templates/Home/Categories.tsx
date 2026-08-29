676017d1ab4b555d27a0e5f4df472ad31ae17d28
Milad salamian <miladsalami1385@gmail.com>
2026-08-29 11:48:47 +0330

    get and show categories in Categories component

--- src/components/templates/Home/Categories.tsx
+++ src/components/templates/Home/Categories.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import CategoriesSlider from "./CategoriesSlider";
 import Title from "@/components/modules/main/Title";
+import { getAllCategories } from "@/services/categories.service";
 
-function Categories() {
+async function Categories() {
+  const categories = await getAllCategories();
+
   return (
     <section className="py-10 relative">
       <Title title="دسته بندی محصولات" />
-      <CategoriesSlider />
+      <CategoriesSlider categories={categories} />
       {/* <div className="hidden lg:block absolute  top-40 left-0 -z-50 w-[0.01px] h-[0.01px] bg-[#ece169] rounded-full shadow-[0px_0px_190px_150px_#ece169]"></div> */}
       {/* <div className="hidden lg:block absolute  top-96 right-0 -z-50 w-[0.01px] h-[0.01px] bg-[#ece169] rounded-full shadow-[0px_0px_190px_150px_#ece169]"></div> */}
     </section>
