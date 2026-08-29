3a68f7cf913fcc62ae992951603770cd712e5567
Milad salamian <miladsalami1385@gmail.com>
2026-08-29 16:25:52 +0330

    add deleteCategoryHandler

--- src/components/templates/p-admin/categories/CategoryRow.tsx
+++ src/components/templates/p-admin/categories/CategoryRow.tsx
@@ -1,8 +1,14 @@
+"use client";
+import ConfirmModal from "@/components/modules/main/ConfirmModal";
+import Modal from "@/components/modules/main/Modal";
 import Table from "@/components/modules/p-admin/Table";
+import { deleteCategory } from "@/libs/actions/category.actions";
 import { ICategory } from "@/libs/types";
 import { formatDate } from "@/utils/helper";
 import Image from "next/image";
 import Link from "next/link";
+import { useTransition } from "react";
+import toast from "react-hot-toast";
 import { FaEye, FaTrash } from "react-icons/fa";
 import { FaPencil } from "react-icons/fa6";
 
@@ -14,7 +20,26 @@ function CategoryRow({
   image,
   createdAt,
   parent,
+  _id,
+  onDelete,
 }: ICategory) {
+  const [isPending, startTransition] = useTransition();
+  const deleteCategoryHandler = () => {
+    startTransition(async () => {
+      if (!_id) return;
+      try {
+        onDelete(_id);
+        const result = await deleteCategory(_id);
+        if (result.success) {
+          toast.success(result.message);
+        } else {
+          toast.error(result.message);
+        }
+      } catch (error) {
+        toast.error("خطا در ارتباط با سرور");
+      }
+    });
+  };
   return (
     <Table.Row>
       <td>{index}</td>
@@ -37,7 +62,18 @@ function CategoryRow({
       <td>
         <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
           <FaPencil className="text-yellow-500 text-base md:text-xl" />
-          <FaTrash className="text-red-600 text-base md:text-xl" />
+          <Modal>
+            <Modal.Open name="delete">
+              <FaTrash className="text-red-600 text-base md:text-xl" />
+            </Modal.Open>
+            <Modal.Page name="delete">
+              <ConfirmModal
+                status="حذف کردن"
+                onSubmit={deleteCategoryHandler}
+                isLoading={isPending}
+              />
+            </Modal.Page>
+          </Modal>
           <Link href={`/category/${href}`}>
             <FaEye className="text-sky-500 text-base md:text-xl" />
           </Link>
