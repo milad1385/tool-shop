f8f330348c40aac605a85e497c2823d82f868363
Milad salamian <miladsalami1385@gmail.com>
2026-08-30 01:20:52 +0330

    add updateCategorySchema validation

--- src/validators/backend/category.validator.ts
+++ src/validators/backend/category.validator.ts
@@ -32,4 +32,29 @@ export const createCategorySchema = z.object({
     ),
 });
 
+export const updateCategorySchema = z.object({
+  name: z
+    .string()
+    .min(3, "نام دسته بندی حداقل ۳ کاراکتر باید باشد")
+    .max(100, "نام دسته بندی حداکثر ۱۰۰ کاراکتر باید باشد"),
+
+  href: z
+    .string()
+    .min(3, "لینک دسته بندی حداقل ۳ کاراکتر باید باشد")
+    .max(100, "لینک دسته بندی حداکثر ۱۰۰ کاراکتر باید باشد")
+    .regex(/^[A-Za-z0-9-]+$/, "لینک فقط شامل حروف کوچک، اعداد و خط تیره باشد"),
+
+  desc: z
+    .string()
+    .min(10, "توضیحات حداقل ۱۰ کاراکتر باید باشد")
+    .max(1000, "توضیحات حداکثر ۱۰۰۰ کاراکتر باید باشد"),
+
+  category: z.string().optional(),
+
+  tags: z.string().min(1, "وارد کردن حداقل یک تگ الزامی است"),
+
+  image: z.any(),
+});
+
 export type TCategorySchema = z.infer<typeof createCategorySchema>;
+export type TUpdateCategorySchema = z.infer<typeof updateCategorySchema>;
