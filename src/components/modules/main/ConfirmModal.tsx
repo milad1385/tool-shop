6589f7206377709ffcbf2b59917a6b9c407f1a2c
Milad salamian <miladsalami1385@gmail.com>
2026-08-29 16:26:40 +0330

    add submit handler in confirm modal

--- src/components/modules/main/ConfirmModal.tsx
+++ src/components/modules/main/ConfirmModal.tsx
@@ -1,9 +1,16 @@
 import Button from "@/components/ui/Button";
 import { IModal } from "@/libs/types";
 import React from "react";
+import { FaSpinner } from "react-icons/fa";
 import { FaXmark } from "react-icons/fa6";
 
-function ConfirmModal({ onClose , isQuestion ,status}: IModal) {
+function ConfirmModal({
+  onClose,
+  isQuestion,
+  status,
+  isLoading,
+  onSubmit,
+}: IModal) {
   return (
     <div className="w-[340px] md:w-[500px] rounded-md bg-white px-4 py-5">
       <div className="flex items-center justify-between border-b-2 border-b-gray-200 pb-4">
@@ -17,18 +24,18 @@ function ConfirmModal({ onClose , isQuestion ,status}: IModal) {
         <p
           className={`text-sm md:text-xl font-bold pt-6 pb-8 p-2 text-justify`}
         >
-          آیا از {status}  اطمینان دارید ؟
+          آیا از {status} اطمینان دارید ؟
         </p>
 
         <div className="flex items-center gap-x-8">
           <Button
-            className="!w-[200px] !bg-green-600 !rounded-lg"
-            onClick={() => onClose()}
+            className="!w-[200px] !bg-green-600 !rounded-lg flex-center h-[48px]"
+            onClick={onSubmit}
           >
-            بله
+            {isLoading ? <FaSpinner className="animate-spin h-5 w-5" /> : "بله"}
           </Button>
           <Button
-            className="!w-[200px] !bg-red-600 !rounded-lg"
+            className="!w-[200px] !bg-red-600 !rounded-lg h-[48px]"
             onClick={() => onClose()}
           >
             خیر
