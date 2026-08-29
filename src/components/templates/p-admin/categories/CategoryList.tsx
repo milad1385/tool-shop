11d94a8f9abe6cd0b747411c676a7a053e6e9d25
Milad salamian <miladsalami1385@gmail.com>
2026-08-29 16:26:18 +0330

    add optimistic ui in categories list

--- src/components/templates/p-admin/categories/CategoryList.tsx
+++ src/components/templates/p-admin/categories/CategoryList.tsx
@@ -1,10 +1,18 @@
+"use client";
 import Pagination from "@/components/modules/p-admin/Pagination";
 import Table from "@/components/modules/p-admin/Table";
-import { ICategoryList } from "@/libs/types";
+import { ICategory, ICategoryList } from "@/libs/types";
 import CategoryRow from "./CategoryRow";
 import { categoryTableHeader } from "@/constants/data";
+import { useOptimistic } from "react";
 
 function CategoryList({ data, pagination }: ICategoryList) {
+  const [optimisticCategory, deleteOptimistc] = useOptimistic(
+    data,
+    (allCategories, id) => {
+      return allCategories.filter((cat: ICategory) => cat._id !== id);
+    },
+  );
   return (
     <div className="md:section-box">
       <div className="admin-table mt-5 overflow-hidden  rounded-md">
@@ -15,8 +23,13 @@ function CategoryList({ data, pagination }: ICategoryList) {
             ))}
           </Table.Header>
           <Table.Body>
-            {data.map((category, index) => (
-              <CategoryRow key={category._id} {...category} index={index + 1} />
+            {optimisticCategory.map((category, index) => (
+              <CategoryRow
+                key={category._id}
+                {...category}
+                index={index + 1}
+                onDelete={deleteOptimistc}
+              />
             ))}
           </Table.Body>
         </Table>
