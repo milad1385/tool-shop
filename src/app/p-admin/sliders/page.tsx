e62e2d2095b07c1ca193a3874210c29c035f2fde
Milad salamian <miladsalami1385@gmail.com>
2026-08-31 01:34:25 +0330

    get and show sliders list in p-admin

--- src/app/p-admin/sliders/page.tsx
+++ src/app/p-admin/sliders/page.tsx
@@ -4,14 +4,25 @@ import TableOperation from "@/components/modules/p-admin/TableOpration";
 import CreateSlider from "@/components/templates/p-admin/sliders/CreateSlider";
 import SliderList from "@/components/templates/p-admin/sliders/SliderList";
 import { sliderFilterOptions } from "@/constants/data";
+import { IPage } from "@/libs/types";
+import { getSliders } from "@/services/sliders.service";
 
-function page() {
+async function page({ searchParams }: IPage) {
+  const params = await searchParams;
+  const page = Number(params.page) || 1;
+  const limit = Number(params.limit) || 10;
+  const search = params.q;
+
+  const sliders = await getSliders({ page, limit, search });
   return (
     <Container>
       <PageTitle content="ایجاد اسلایدر" />
-      <CreateSlider/>
-      <TableOperation pageTitle="لیست اسلایدر ها" options={sliderFilterOptions}/>
-      <SliderList/>
+      <CreateSlider />
+      <TableOperation
+        pageTitle="لیست اسلایدر ها"
+        options={sliderFilterOptions}
+      />
+      <SliderList data={sliders.data} pagination={sliders.pagination} />
     </Container>
   );
 }
