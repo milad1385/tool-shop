1d61524eb6ba186d55523b48bac30a120c88b5ac
Milad salamian <miladsalami1385@gmail.com>
2026-08-31 01:33:54 +0330

    get and show slider row

--- src/components/templates/p-admin/sliders/sliderRow.tsx
+++ src/components/templates/p-admin/sliders/sliderRow.tsx
@@ -1,17 +1,37 @@
 import Table from "@/components/modules/p-admin/Table";
+import { ISlider } from "@/libs/types";
+import { formatDate } from "@/utils/helper";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import { FaTrash } from "react-icons/fa";
 import { FaPencil, FaXmark } from "react-icons/fa6";
 
-function sliderRow() {
+function SliderRow({
+  index,
+  title,
+  href,
+  image,
+  priority,
+  status,
+  createdAt,
+}: ISlider) {
+  console.log(status);
+
+  const getMainBgColor = (status) => {
+    if (status === "ACCEPT") {
+      return { color: "bg-green-600", title: "تایید شده" };
+    } else if (status === "PENDING") {
+      return { color: "bg-yellow-500", title: "در حال بررسی" };
+    } else {
+      return { color: "bg-red-600", title: "رد شده" };
+    }
+  };
   return (
     <Table.Row>
-      <td>1</td>
+      <td>{index}</td>
       <td className="!p-0 md:!p-5">
         <Image
-          src="/images/slider-2.jpg"
+          src={image}
           className="w-[100px] h-[50px] md:w-[200px] md:h-[90px] my-2 md:my-0 object-cover rounded-md mx-auto"
           alt="slider-2.jpg"
           width={1920}
@@ -19,14 +39,16 @@ function sliderRow() {
         />
       </td>
       <td>
-        <Link href="/products">اسلایدر اول</Link>
+        <Link href={href}>{title}</Link>
       </td>
 
-      <td>1404/04/25</td>
-      <td>1</td>
+      <td>{formatDate(createdAt)}</td>
+      <td>{priority}</td>
       <td>
-        <div className="bg-green-500 text-white rounded-3xl py-2">
-          منتشر شده
+        <div
+          className={`${getMainBgColor(status).color} text-white rounded-3xl py-2`}
+        >
+          {getMainBgColor(status).title}
         </div>
       </td>
       <td>
@@ -41,4 +63,4 @@ function sliderRow() {
   );
 }
 
-export default sliderRow;
+export default SliderRow;
