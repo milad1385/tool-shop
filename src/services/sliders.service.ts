3b6e63cdedff9fcc5b15831ebb36f6ea49a8128e
Milad salamian <miladsalami1385@gmail.com>
2026-08-31 01:32:57 +0330

    add getSliders

--- src/services/sliders.service.ts
+++ src/services/sliders.service.ts
@@ -1,14 +1,46 @@
 import connectToDB from "@/configs/db";
-import { ISlider } from "@/libs/types";
+import {
+    IGetSliders,
+    IPaginatedResponse,
+    ISlider
+} from "@/libs/types";
 import Slider from "@/models/Slider";
-import { normalizeData } from "@/utils/helper";
+import { createPagination, normalizeData } from "@/utils/helper";
 
-export const getAllSliders = async () : Promise<ISlider[]> => {
+export const getAllSliders = async (): Promise<ISlider[]> => {
   try {
     await connectToDB();
     const sliders = await Slider.find({}).sort({ priority: 1 });
     return normalizeData(sliders);
   } catch (err) {
     throw new Error(err?.message);
+  }
+};
+
+export const getSliders = async ({
+  page = 1,
+  limit = 10,
+  search = "",
+}: IGetSliders = {}): Promise<IPaginatedResponse<ISlider>> => {
+  try {
+    await connectToDB();
+    let filters: any = {};
+
+    const count = await Slider.countDocuments({});
+
+    if (search) {
+      filters.title = { $regex: search };
+    }
+    const sliders = await Slider.find(filters)
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .sort({ createdAt: -1 });
+
+    return {
+      data: normalizeData(sliders),
+      pagination: createPagination({ page, limit, count }),
+    };
+  } catch (error) {
+    throw new Error(error.message);
   }
 };
