1c7f40db22806ffe2f8000dbdaf427396266b245
Milad salamian <miladsalami1385@gmail.com>
2026-08-31 01:34:05 +0330

    generate slider list

--- src/components/templates/p-admin/sliders/SliderList.tsx
+++ src/components/templates/p-admin/sliders/SliderList.tsx
@@ -1,214 +1,26 @@
 import Pagination from "@/components/modules/p-admin/Pagination";
 import Table from "@/components/modules/p-admin/Table";
-import Image from "next/image";
-import Link from "next/link";
-import { FaTrash } from "react-icons/fa";
-import { FaPencil, FaXmark } from "react-icons/fa6";
+import { ISliderList } from "@/libs/types";
+import SliderRow from "./SliderRow";
+import { sliderTableHeader } from "@/constants/data";
 
-function SliderList() {
+function SliderList({ data, pagination }: ISliderList) {
   return (
     <div className="md:section-box">
       <div className="admin-table mt-5 overflow-hidden  rounded-md">
         <Table>
           <Table.Header>
-            <th>شماره</th>
-            <th>عکس</th>
-            <th>عنوان</th>
-            <th>تاریخ</th>
-            <th>اولویت</th>
-            <th>وضعیت</th>
-            <th>عملیات</th>
+            {sliderTableHeader.map((header, index) => (
+              <th key={index + 1}>{header}</th>
+            ))}
           </Table.Header>
           <Table.Body>
-            <Table.Row>
-              <td>1</td>
-              <td className="!p-0 md:!p-5">
-                <Image
-                  src="/images/slider-2.jpg"
-                  className="w-[100px] h-[50px] md:w-[200px] md:h-[90px] my-2 md:my-0 object-cover rounded-md mx-auto"
-                  alt="slider-2.jpg"
-                  width={1920}
-                  height={1080}
-                />
-              </td>
-              <td>
-                <Link href="/products">اسلایدر اول</Link>
-              </td>
-
-              <td>1404/04/25</td>
-              <td>1</td>
-              <td>
-                <div className="bg-green-500 text-white rounded-3xl py-2">
-                  منتشر شده
-                </div>
-              </td>
-              <td>
-                <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
-                  <FaPencil className="text-sky-500 text-base md:text-xl" />
-                  <FaTrash className="text-red-600 text-base md:text-xl" />
-
-                  <FaXmark className="text-yellow-500 text-base md:text-2xl" />
-                </div>
-              </td>
-            </Table.Row>
-            <Table.Row>
-              <td>2</td>
-              <td className="!p-0 md:!p-5">
-                <Image
-                  src="/images/slider-2.jpg"
-                  className="w-[100px] h-[50px] md:w-[200px] md:h-[90px] my-2 md:my-0 object-cover rounded-md mx-auto"
-                  alt="slider-2.jpg"
-                  width={1920}
-                  height={1080}
-                />
-              </td>
-              <td>
-                <Link href="/products">اسلایدر اول</Link>
-              </td>
-
-              <td>1404/04/25</td>
-              <td>1</td>
-              <td>
-                <div className="bg-red-500 text-white rounded-3xl py-2">
-                  منتشر نشده
-                </div>
-              </td>
-              <td>
-                <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
-                  <FaPencil className="text-sky-500 text-base md:text-xl" />
-                  <FaTrash className="text-red-600 text-base md:text-xl" />
-
-                  <FaXmark className="text-yellow-500 text-base md:text-2xl" />
-                </div>
-              </td>
-            </Table.Row>
-            <Table.Row>
-              <td>3</td>
-              <td className="!p-0 md:!p-5">
-                <Image
-                  src="/images/slider-2.jpg"
-                  className="w-[100px] h-[50px] md:w-[200px] md:h-[90px] my-2 md:my-0 object-cover rounded-md mx-auto"
-                  alt="slider-2.jpg"
-                  width={1920}
-                  height={1080}
-                />
-              </td>
-              <td>
-                <Link href="/products">اسلایدر اول</Link>
-              </td>
-
-              <td>1404/04/25</td>
-              <td>1</td>
-              <td>
-                <div className="bg-green-500 text-white rounded-3xl py-2">
-                  منتشر شده
-                </div>
-              </td>
-              <td>
-                <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
-                  <FaPencil className="text-sky-500 text-base md:text-xl" />
-                  <FaTrash className="text-red-600 text-base md:text-xl" />
-
-                  <FaXmark className="text-yellow-500 text-base md:text-2xl" />
-                </div>
-              </td>
-            </Table.Row>
-            <Table.Row>
-              <td>4</td>
-              <td className="!p-0 md:!p-5">
-                <Image
-                  src="/images/slider-2.jpg"
-                  className="w-[100px] h-[50px] md:w-[200px] md:h-[90px] my-2 md:my-0 object-cover rounded-md mx-auto"
-                  alt="slider-2.jpg"
-                  width={1920}
-                  height={1080}
-                />
-              </td>
-              <td>
-                <Link href="/products">اسلایدر اول</Link>
-              </td>
-
-              <td>1404/04/25</td>
-              <td>1</td>
-             <td>
-                <div className="bg-red-500 text-white rounded-3xl py-2">
-                  منتشر نشده
-                </div>
-              </td>
-              <td>
-                <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
-                  <FaPencil className="text-sky-500 text-base md:text-xl" />
-                  <FaTrash className="text-red-600 text-base md:text-xl" />
-
-                  <FaXmark className="text-yellow-500 text-base md:text-2xl" />
-                </div>
-              </td>
-            </Table.Row>
-            <Table.Row>
-              <td>5</td>
-              <td className="!p-0 md:!p-5">
-                <Image
-                  src="/images/slider-2.jpg"
-                  className="w-[100px] h-[50px] md:w-[200px] md:h-[90px] my-2 md:my-0 object-cover rounded-md mx-auto"
-                  alt="slider-2.jpg"
-                  width={1920}
-                  height={1080}
-                />
-              </td>
-              <td>
-                <Link href="/products">اسلایدر اول</Link>
-              </td>
-
-              <td>1404/04/25</td>
-              <td>1</td>
-              <td>
-                <div className="bg-green-500 text-white rounded-3xl py-2">
-                  منتشر شده
-                </div>
-              </td>
-              <td>
-                <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
-                  <FaPencil className="text-sky-500 text-base md:text-xl" />
-                  <FaTrash className="text-red-600 text-base md:text-xl" />
-
-                  <FaXmark className="text-yellow-500 text-base md:text-2xl" />
-                </div>
-              </td>
-            </Table.Row>
-            <Table.Row>
-              <td>6</td>
-              <td className="!p-0 md:!p-5">
-                <Image
-                  src="/images/slider-2.jpg"
-                  className="w-[100px] h-[50px] md:w-[200px] md:h-[90px] my-2 md:my-0 object-cover rounded-md mx-auto"
-                  alt="slider-2.jpg"
-                  width={1920}
-                  height={1080}
-                />
-              </td>
-              <td>
-                <Link href="/products">اسلایدر اول</Link>
-              </td>
-
-              <td>1404/04/25</td>
-              <td>1</td>
-              <td>
-                <div className="bg-red-500 text-white rounded-3xl py-2">
-                  منتشر نشده
-                </div>
-              </td>
-              <td>
-                <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
-                  <FaPencil className="text-sky-500 text-base md:text-xl" />
-                  <FaTrash className="text-red-600 text-base md:text-xl" />
-
-                  <FaXmark className="text-yellow-500 text-base md:text-2xl" />
-                </div>
-              </td>
-            </Table.Row>
+            {data.map((slide, index) => (
+              <SliderRow key={slide._id} index={index + 1} {...slide} />
+            ))}
           </Table.Body>
         </Table>
-        <Pagination count={35} />
+        <Pagination count={pagination.totalItems} />
       </div>
     </div>
   );
