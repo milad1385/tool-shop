c43128c6e32ce821972b2c4f68f400361ddfd5b8
Milad salamian <miladsalami1385@gmail.com>
2026-08-31 12:52:49 +0330

    add optmistic ui in slider list

--- src/components/templates/p-admin/sliders/SliderList.tsx
+++ src/components/templates/p-admin/sliders/SliderList.tsx
@@ -1,11 +1,19 @@
+"use client";
 import Pagination from "@/components/modules/p-admin/Pagination";
 import Table from "@/components/modules/p-admin/Table";
-import { ISliderList } from "@/libs/types";
+import { ISlider, ISliderList } from "@/libs/types";
 import SliderRow from "./SliderRow";
 import { sliderTableHeader } from "@/constants/data";
 import EmptyError from "@/components/modules/p-admin/EmptyError";
+import { useOptimistic } from "react";
 
 function SliderList({ data, pagination }: ISliderList) {
+  const [optimisticSliders, deleteOptimistc] = useOptimistic(
+    data,
+    (allSliders, id) => {
+      return allSliders.filter((slider: ISlider) => slider._id !== id);
+    },
+  );
   return (
     <div className="md:section-box">
       <div className="admin-table mt-5 overflow-hidden  rounded-md">
@@ -16,8 +24,13 @@ function SliderList({ data, pagination }: ISliderList) {
             ))}
           </Table.Header>
           <Table.Body>
-            {data.map((slide, index) => (
-              <SliderRow key={slide._id} index={index + 1} {...slide} />
+            {optimisticSliders.map((slide, index) => (
+              <SliderRow
+                key={slide._id}
+                index={index + 1}
+                onDelete={deleteOptimistc}
+                {...slide}
+              />
             ))}
           </Table.Body>
         </Table>
