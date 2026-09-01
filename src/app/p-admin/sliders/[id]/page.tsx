350bab2718e25a653b3edaf5c2f95903cbbb4756
Milad salamian <miladsalami1385@gmail.com>
2026-09-01 12:46:16 +0330

    get and show edit slider info

--- src/app/p-admin/sliders/[id]/page.tsx
+++ src/app/p-admin/sliders/[id]/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Container from "@/components/modules/p-admin/Container";
+import PageTitle from "@/components/modules/p-admin/PageTitle";
+import UpdateSlider from "@/components/templates/p-admin/sliders/UpdateSlider";
+import { IPage } from "@/libs/types";
+import { getOneSlider } from "@/services/sliders.service";
+import React from "react";
 
-function page() {
+async function page({ params }: IPage) {
+  const { id } = await params;
+  const slider = await getOneSlider(id);
   return (
-    <div>page</div>
-  )
+    <Container>
+      <PageTitle content={`ویرایش اسلایدر ${slider.title}`} />
+      <UpdateSlider slider={slider} />
+    </Container>
+  );
 }
 
-export default page
+export default page;
