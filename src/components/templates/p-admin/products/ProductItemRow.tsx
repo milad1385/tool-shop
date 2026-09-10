155728a2406f53e124d7074a7b12fffef7815bf1
Milad salamian <miladsalami1385@gmail.com>
2026-09-11 00:45:23 +0330

    get and show product info in product row component

--- src/components/templates/p-admin/products/ProductItemRow.tsx
+++ src/components/templates/p-admin/products/ProductItemRow.tsx
@@ -1,32 +1,44 @@
 import Table from "@/components/modules/p-admin/Table";
+import { IProduct } from "@/libs/types";
+import { formatDate } from "@/utils/helper";
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
 import { FaEye, FaTrash } from "react-icons/fa";
 import { FaPencil } from "react-icons/fa6";
 
-function ProductItemRow() {
+function ProductItemRow({
+  index,
+  images,
+  name,
+  slug,
+  category,
+  createdAt,
+}: IProduct) {
   return (
     <Table.Row>
-      <td>1</td>
+      <td>{index}</td>
       <td className="!p-0 md:!p-5">
         <Image
-          src="/images/product-7.jpg"
+          src={images[0]}
           className="w-20 rounded-md mx-auto my-2 md:my-0"
-          alt=""
+          alt={name}
           width={1920}
           height={1080}
         />
       </td>
-      <td>دریل رونیکس</td>
-      <td>Ronix</td>
-      <td>دریل</td>
-      <td>رونیکس ، ابزار</td>
-      <td>1404/04/25</td>
+      <td>
+        <Link href={`/products/${slug}`}>{name}</Link>
+      </td>
+      <td>{slug}</td>
+      <td>{category.name}</td>
+      <td>{formatDate(createdAt)}</td>
       <td>
         <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
           <FaPencil className="text-yellow-500 text-base md:text-xl" />
           <FaTrash className="text-red-600 text-base md:text-xl" />
-          <FaEye className="text-sky-500 text-base md:text-xl" />
+          <Link href={`/products/${slug}`}>
+            <FaEye className="text-sky-500 text-base md:text-xl" />
+          </Link>
         </div>
       </td>
     </Table.Row>
