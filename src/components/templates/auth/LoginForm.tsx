472d9a1b3293eb66f5dd27b7ac8c85fa2558358f
Milad salamian <miladsalami1385@gmail.com>
2026-09-11 16:33:43 +0330

    add login with google in login form component

--- src/components/templates/auth/LoginForm.tsx
+++ src/components/templates/auth/LoginForm.tsx
@@ -1,18 +1,19 @@
 "use client";
 import Input from "@/components/ui/Input";
+import { loginUser as loginAction } from "@/libs/actions/auth.actions";
+import { useAuthStore } from "@/stores/auth.store";
 import {
   userLogin,
   userLoginType,
 } from "@/validators/frontend/user/user.validator";
 import { yupResolver } from "@hookform/resolvers/yup";
-import React from "react";
-import { useForm } from "react-hook-form";
-import { loginUser as loginAction } from "@/libs/actions/auth.actions";
+import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { FaSpinner } from "react-icons/fa";
-import { useTransition } from "react";
-import { useAuthStore } from "@/stores/auth.store";
+import { FcGoogle } from "react-icons/fc";
 
 function LoginForm() {
   const router = useRouter();
@@ -81,10 +82,19 @@ function LoginForm() {
       <button
         type="submit"
         disabled={isPending}
-        className="p-3 rounded-md bg-stone-800 hover:bg-stone-900 text-white w-full my-4 disabled:opacity-50 disabled:cursor-not-allowed transition-colors flex items-center justify-center gap-2 h-[48px]"
+        className="p-3 rounded-md bg-stone-800 hover:bg-stone-900 text-white w-full mt-4 mb-0 disabled:opacity-50 disabled:cursor-not-allowed transition-colors flex items-center justify-center gap-2 h-[48px]"
       >
         {isPending ? <FaSpinner className="animate-spin h-5 w-5" /> : "ورود"}
       </button>
+      <div className="bg-gray-200  py-2.5 rounded-md cursor-pointer">
+        <div className="flex items-center gap-x-2 justify-center">
+          <span className="text-sm">ورود با گوگل</span>
+          <FcGoogle
+            onClick={() => signIn("google", { callbackUrl: "/" })}
+            className="text-[28px] cursor-pointer"
+          />
+        </div>
+      </div>
     </form>
   );
 }
