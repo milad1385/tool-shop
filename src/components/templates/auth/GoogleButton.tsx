d0f605213f18d68c578dd4abfc066d1e01e746ed
Milad salamian <miladsalami1385@gmail.com>
2026-09-11 16:41:24 +0330

    add google button component

---
+++ src/components/templates/auth/GoogleButton.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { IGoogleButton } from "@/libs/types";
+import { signIn } from "next-auth/react";
+import { FcGoogle } from "react-icons/fc";
+
+function GoogleButton({ text }: IGoogleButton) {
+  return (
+    <div
+      className="bg-gray-200  py-2.5 rounded-md cursor-pointer"
+      onClick={() => signIn("google", { callbackUrl: "/" })}
+    >
+      <div className="flex items-center gap-x-2 justify-center">
+        <span className="text-sm">{text}</span>
+        <FcGoogle className="text-[28px] cursor-pointer" />
+      </div>
+    </div>
+  );
+}
+
+export default GoogleButton;
