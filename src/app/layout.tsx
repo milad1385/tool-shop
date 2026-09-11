d66f85b8b1bb9d0a941273e52844465c496844bb
Milad salamian <miladsalami1385@gmail.com>
2026-09-11 16:35:38 +0330

    init auth initializer in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,24 @@
-import Navbar from "@/components/modules/main/Navbar";
-import type { Metadata } from "next";
-import "./globals.css";
 import Footer from "@/components/modules/main/Footer";
+import Navbar from "@/components/modules/main/Navbar";
+import AuthProvider from "@/providers/AuthProvider";
+import type { Metadata } from "next";
 import NextTopLoader from "nextjs-toploader";
 import { Toaster } from "react-hot-toast";
-import AuthProvider from "@/providers/AuthProvider";
+import "./globals.css";
+import { SessionProvider } from "next-auth/react";
+import AuthInitializer from "@/providers/AuthInitializer";
 
 export const metadata: Metadata = {
   title: {
     template: "%s | ابزار آلات ترازو",
     default: "ابزار آلات | ترازو",
   },
-  description:
-    "سایت ابزار آلات میلاد مکانی برای خرید انواع ابزار های صنعتی و خانگی میباشد ، در این سایت شما امکان خرید انواع دریل ، هیلتی و... را در دارید",
-  icons: {
-    icon: "/images/tool.png",
-  },
-  openGraph: {
-    title: "ابزار آلات ترازو",
-  },
+  description: "...",
+  icons: { icon: "/images/tool.png" },
+  openGraph: { title: "ابزار آلات ترازو" },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -30,12 +27,16 @@ export default function RootLayout({
     <html lang="fa" dir="rtl" className="overflow-x-hidden select-none">
       <body className={`font-dana bg-gray-100 antialiased overflow-x-hidden`}>
         <NextTopLoader color="#eab308" showSpinner={false} />
-        <AuthProvider>
-          <Navbar />
-          {children}
-          <Footer />
-          <Toaster />
-        </AuthProvider>
+        <SessionProvider>
+          <AuthInitializer>
+            <AuthProvider>
+              <Navbar />
+              {children}
+              <Footer />
+              <Toaster />
+            </AuthProvider>
+          </AuthInitializer>
+        </SessionProvider>
       </body>
     </html>
   );
