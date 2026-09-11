4e8fe33d6eb1f098cbc6c52182c1ae98301999fc
Milad salamian <miladsalami1385@gmail.com>
2026-09-11 16:34:22 +0330

    add session checker in proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,36 +1,52 @@
 import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/auth";
 import { UserRoleEnums } from "./libs/types";
 import { verifyToken } from "./utils/helper";
-export function proxy(request: NextRequest) {
+
+export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   const accessToken = request.cookies.get("accessToken");
 
   if (pathname.startsWith("/auth")) {
-    if (accessToken) {
+    const session = await auth();
+    if (accessToken || session) {
       return NextResponse.redirect(new URL("/", request.url));
     }
+    return NextResponse.next();
   }
+
 
   if (pathname.startsWith("/p-user")) {
     if (!accessToken) {
       return NextResponse.redirect(new URL("/", request.url));
     }
+    return NextResponse.next();
   }
 
   if (pathname.startsWith("/p-admin")) {
     if (!accessToken) {
       return NextResponse.redirect(new URL("/", request.url));
     }
-    const user = verifyToken(accessToken.value);
 
-    const isAdmin = user.roles.some((role) =>
-      [UserRoleEnums.SUPER_ADMIN, UserRoleEnums.ADMIN].includes(role),
-    );
-    if (!isAdmin) {
+    try {
+      const user = verifyToken(accessToken.value);
+      const isAdmin = user.roles.some((role: UserRoleEnums) =>
+        [UserRoleEnums.SUPER_ADMIN, UserRoleEnums.ADMIN].includes(role)
+      );
+
+      if (!isAdmin) {
+        return NextResponse.redirect(new URL("/", request.url));
+      }
+    } catch (error) {
       return NextResponse.redirect(new URL("/", request.url));
     }
+    return NextResponse.next();
   }
+
+  return NextResponse.next();
 }
+
+export default proxy;
 
 export const config = {
   matcher: ["/auth/:path*", "/p-user/:path*", "/p-admin/:path*"],
