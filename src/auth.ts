a0c6e2775e80f490ccf343a1b387f25f71a18e0a
Milad salamian <miladsalami1385@gmail.com>
2026-09-11 16:29:08 +0330

    add auth.ts for next auth

---
+++ src/auth.ts
@@ -0,0 +1,69 @@
+import { UserRoleEnums } from "@/libs/types";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { createGoogleUser } from "./utils/auth";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/auth",
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      try {
+        if (account?.provider === "google") {
+          const existingUser = await createGoogleUser({
+            email: user.email?.toLowerCase().trim() || "",
+            name: user.name,
+            image: user.image,
+            providerId: profile?.sub,
+          });
+
+          if (existingUser.status === "banned") {
+            return false;
+          }
+
+          user.id = existingUser._id.toString();
+          (user as any).roles = existingUser.roles.map((r: any) =>
+            r.toString(),
+          );
+          (user as any).fullname = existingUser.fullname?.toString() || "";
+          (user as any).username = existingUser.username?.toString() || "";
+        }
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+
+    async jwt({ token, user, account }) {
+      if (user) {
+        token.id = user.id;
+        token.roles = [UserRoleEnums.USER];
+        token.fullname = (user as any).fullname;
+        token.username = (user as any).username;
+        token.provider = account?.provider;
+      }
+      return token;
+    },
+
+    async session({ session, token }: any) {
+      if (token && session.user) {
+        session.user.id = token.id as string;
+        session.user.roles = token.roles as string[];
+        (session.user as any).fullname = token.fullname;
+        (session.user as any).username = token.username;
+      }
+      return session;
+    },
+  },
+});
