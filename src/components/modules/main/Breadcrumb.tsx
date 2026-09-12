889dda76a71ea97c9b517c0b76f6f0fdb4caa500
Milad salamian <miladsalami1385@gmail.com>
2026-09-13 00:03:39 +0330

    fix bread crumb style bug

--- src/components/modules/main/Breadcrumb.tsx
+++ src/components/modules/main/Breadcrumb.tsx
@@ -4,20 +4,22 @@ import { HiChevronLeft } from "react-icons/hi2";
 
 function Breadcrumb({ links }: IBreadcrumb) {
   return (
-    <div className="bg-white my-6  p-5 rounded-2xl">
-      <div className="flex items-center gap-x-2 w-[372px] md:w-[450px]">
+    <div className="bg-white my-6 p-5 rounded-2xl">
+      <div className="flex items-center gap-x-2 overflow-x-auto whitespace-nowrap no-scrollbar">
         {links.map((link) => (
           <Link
             key={link.id}
             href={link.href}
-            className={`flex items-center gap-x-1 text-sm lg:text-base ${
+            className={`flex items-center gap-x-1 text-sm lg:text-base shrink-0 ${
               link.id !== links.length
                 ? "text-zinc-700"
                 : "text-yellow-500 font-DanaMedium"
-            } `}
+            }`}
           >
             {link.name}
-            {link.id !== links.length ? <HiChevronLeft className="text-lg" /> : null}
+            {link.id !== links.length ? (
+              <HiChevronLeft className="text-lg" />
+            ) : null}
           </Link>
         ))}
       </div>
