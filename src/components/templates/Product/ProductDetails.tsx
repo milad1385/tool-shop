e5b791c313b3dd1311957aced4e46ad5018699a0
Milad salamian <miladsalami1385@gmail.com>
2026-09-13 00:04:10 +0330

    get and show product price in product detail component

--- src/components/templates/Product/ProductDetails.tsx
+++ src/components/templates/Product/ProductDetails.tsx
@@ -1,4 +1,7 @@
+// components/templates/Product/ProductDetails.tsx
 import ColorBoxes from "@/components/modules/main/ColorBoxes";
+import { IProduct } from "@/libs/types";
+import { formattedPrice } from "@/utils/helper";
 import { FaRegCreditCard } from "react-icons/fa";
 import { FaRegFaceSmile } from "react-icons/fa6";
 import { FiGift } from "react-icons/fi";
@@ -8,15 +11,33 @@ import { MdOutlineSupportAgent } from "react-icons/md";
 import { PiPaperPlaneRight } from "react-icons/pi";
 import AddToCart from "./AddToCart";
 import FactorItem from "./FactorItem";
-import { IProduct } from "@/libs/types";
 
 function ProductDetails({
   name,
   category,
   features,
   customFeatures,
+  sellers,
 }: IProduct) {
   const productFeatures = [...features, ...customFeatures];
+
+  const getFinalPrice = (price: number, discount: number = 0) => {
+    return price - (price * discount) / 100;
+  };
+
+  const cheapestSeller = sellers.reduce((cheapest, current) => {
+    const currentFinal = getFinalPrice(current.price, current.discount);
+    const cheapestFinal = getFinalPrice(cheapest.price, cheapest.discount);
+    return currentFinal < cheapestFinal ? current : cheapest;
+  });
+
+  const originalPrice = cheapestSeller.price;
+  const finalPrice = getFinalPrice(
+    cheapestSeller.price,
+    cheapestSeller.discount,
+  );
+  const discount = cheapestSeller.discount || 0;
+
   return (
     <div className="col-span-12 md:col-span-8">
       <div className="px-1 py-3 md:py-4 md:px-4">
@@ -26,31 +47,45 @@ function ProductDetails({
             دسته بندی: {category.name}
           </p>
         </div>
+
         <div className="mt-5 md:mt-6 px-4">
-          <div className="flex gap-4 text-base mb-4 md:mb-0">
-            <span className="line-through text-zinc-400  text-lg">360.000</span>
-            <div className="flex items-center gap-x-2">
-              <span className="text-black font-bold text-lg md:text-xl">
-                280.000 تومان
+          <div className="flex items-center gap-3">
+            {discount > 0 ? (
+              <>
+                <span className="line-through text-zinc-400 text-lg">
+                  {formattedPrice(originalPrice)}
+                </span>
+
+                <span className="text-black font-bold text-lg md:text-xl">
+                  {formattedPrice(finalPrice)} تومان
+                </span>
+
+                <span className="flex items-center justify-center h-6 bg-yellow-500 text-white px-3 rounded-xl text-sm">
+                  {discount}٪
+                </span>
+              </>
+            ) : (
+              <span className="font-bold text-zinc-800 text-lg">
+                {formattedPrice(finalPrice)} تومان
               </span>
-              <span className="flex-center h-6 bg-yellow-500 text-white px-3 rounded-xl text-sm">
-                ۸٪
-              </span>
-            </div>
+            )}
           </div>
         </div>
+
         <div className="grid grid-cols-12 mt-4 md:mt-2">
           <div className="col-span-12 lg:col-span-8 px-1 py-2 md:py-4 md:px-3">
             <div>
               <h3 className="text-xl font-Lalezar">مشخصات کالا</h3>
-              <div className="grid grid-cols-3 gap-3 mt-3">
+              <div className="grid grid-cols-2 md:grid-cols-3 gap-3 mt-3">
                 {productFeatures.slice(0, 6).map((feature) => (
                   <FactorItem {...feature} key={feature._id} />
                 ))}
               </div>
             </div>
-            <div className="flex items-start flex-col gap-2 md:gap-0 pt-8 md:pt-8">
+
+            <div className="flex items-start flex-col gap-2 md:gap-0 pt-8">
               <ColorBoxes />
+
               <div className="w-full flex items-center gap-x-4">
                 <div className="hidden md:block">
                   <AddToCart />
@@ -60,36 +95,44 @@ function ProductDetails({
                   افزودن به مقایسه
                 </button>
               </div>
+
               <div className="block md:hidden w-full">
                 <AddToCart />
               </div>
             </div>
           </div>
+
           <div className="hidden lg:block col-span-4">
             <div className="flex items-center border rounded-lg my-2 p-2">
               <FaRegCreditCard className="text-xl text-yellow-500" />
               <span className="mr-2">بهترین قیمت</span>
             </div>
+
             <div className="flex items-center border rounded-lg my-2 p-2">
               <IoKeyOutline className="text-2xl text-yellow-500" />
               <span className="mr-2">تضمین اصل بودن محصول</span>
             </div>
+
             <div className="flex items-center border rounded-lg my-2 p-2">
               <PiPaperPlaneRight className="text-2xl text-yellow-500" />
               <span className="mr-2">ارسال سریع</span>
             </div>
+
             <div className="flex items-center border rounded-lg my-2 p-2">
               <MdOutlineSupportAgent className="text-2xl text-yellow-500" />
               <span className="mr-2">مشاوره قبل از خرید</span>
             </div>
+
             <div className="flex items-center border rounded-lg my-2 p-2">
               <FiGift className="text-2xl text-yellow-500" />
               <span className="mr-2">بسته بندی زیبا</span>
             </div>
+
             <div className="flex items-center border rounded-lg my-2 p-2">
               <FaRegFaceSmile className="text-xl text-yellow-500" />
               <span className="mr-2">رضایت کاربران</span>
             </div>
+
             <div className="flex items-center border rounded-lg my-2 p-2">
               <IoMdCheckmark className="text-xl text-yellow-500" />
               <span className="mr-2">تایید فروشنده توسط ترازو</span>
