f9438c5016d3275cc51635230aaa27368628d9fb
Milad salamian <miladsalami1385@gmail.com>
2026-09-12 23:40:09 +0330

    fix feature item bug

--- src/components/templates/Product/FactorItem.tsx
+++ src/components/templates/Product/FactorItem.tsx
@@ -5,10 +5,10 @@ function FactorItem({ name, value }: IFactorItem) {
   return (
     <div className="border border-gray-300  rounded-lg px-3 py-1 md:py-1.5 md:bg-stone-50">
       <div className="flex items-center justify-between gap-x-2">
-        <span className="text-sm font-bold block">{name}</span>
+        <span className="text-sm font-bold block !line-clamp-1">{name}</span>
         <HiChevronLeft />
       </div>
-      <p className="text-gray-500 text-xs font-bold mt-2">{value}</p>
+      <p className="text-gray-500 text-xs font-bold mt-2 line-clamp-1">{value}</p>
     </div>
   );
 }
