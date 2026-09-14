7dd7531c81f1839c0e6810f1e9ed4294088ce927
Milad salamian <miladsalami1385@gmail.com>
2026-09-14 21:01:43 +0330

    get and show user info in sidebar component

--- src/components/modules/p-user/Sidebar.tsx
+++ src/components/modules/p-user/Sidebar.tsx
@@ -1,9 +1,11 @@
+"use client";
 import { links } from "@/constants/data";
 import MenuItem from "./MenuItem";
 import Image from "next/image";
+import { useAuthStore } from "@/stores/auth.store";
 
 function Sidebar() {
- 
+  const { user } = useAuthStore();
   return (
     <div className="col-span-3  hidden lg:block">
       <div className="bg-white rounded-3xl overflow-hidden  xl:w-[312px] fixed">
@@ -11,12 +13,18 @@ function Sidebar() {
           <div className="absolute inset-0 flex items-center justify-center flex-col top-24">
             <div className="avatar online mt-8">
               <div className="w-24 rounded-full overflow-hidden border-4 border-white">
-                <Image src="/images/avatar-3.jpg" alt="user-image" width={1920} height={1080} className="w-24" />
+                <Image
+                  src="/images/user.jpg"
+                  alt="/images/user.jpg"
+                  width={1920}
+                  height={1080}
+                  className="w-24"
+                />
               </div>
             </div>
 
             <h1 className="text-sm md:text-lg font-Lalezar mt-4">
-              میلاد سلامیان
+             {user?.fullname}
             </h1>
           </div>
         </div>
