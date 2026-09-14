e81babf3b6fba19dfe8ab447a1f26d8c6dc9beb2
Milad salamian <miladsalami1385@gmail.com>
2026-09-14 23:59:14 +0330

    fix proxy.ts bug

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,39 +7,53 @@ export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   const accessToken = request.cookies.get("accessToken");
 
+  const session = await auth();
+
   if (pathname.startsWith("/auth")) {
-    const session = await auth();
     if (accessToken || session) {
       return NextResponse.redirect(new URL("/", request.url));
     }
     return NextResponse.next();
   }
 
-
   if (pathname.startsWith("/p-user")) {
-    if (!accessToken) {
+    if (!accessToken && !session) {
       return NextResponse.redirect(new URL("/", request.url));
     }
     return NextResponse.next();
   }
 
   if (pathname.startsWith("/p-admin")) {
-    if (!accessToken) {
+    if (!accessToken && !session) {
       return NextResponse.redirect(new URL("/", request.url));
     }
 
-    try {
-      const user = verifyToken(accessToken.value);
-      const isAdmin = user.roles.some((role: UserRoleEnums) =>
-        [UserRoleEnums.SUPER_ADMIN, UserRoleEnums.ADMIN].includes(role)
-      );
+    let isAdmin = false;
 
-      if (!isAdmin) {
-        return NextResponse.redirect(new URL("/", request.url));
+    if (accessToken) {
+      try {
+        const user = verifyToken(accessToken.value);
+        isAdmin = user.roles.some((role: UserRoleEnums) =>
+          [UserRoleEnums.SUPER_ADMIN, UserRoleEnums.ADMIN].includes(role)
+        );
+      } catch (error) {
+        console.error("خطا در verifyToken:", error);
       }
-    } catch (error) {
+    }
+
+    if (!isAdmin && session?.user) {
+      const roles = (session.user as any).roles || [];
+      isAdmin = roles.some((role: string) =>
+        [UserRoleEnums.SUPER_ADMIN, UserRoleEnums.ADMIN].includes(
+          role as UserRoleEnums
+        )
+      );
+    }
+
+    if (!isAdmin) {
       return NextResponse.redirect(new URL("/", request.url));
     }
+
     return NextResponse.next();
   }
 
