67a6222ab0db723dbeec2da6b186fea153f19549
Milad salamian <miladsalami1385@gmail.com>
2026-09-16 00:00:27 +0330

    get and show contacts list in p-admin

--- src/components/templates/p-admin/contacts/ContactList.tsx
+++ src/components/templates/p-admin/contacts/ContactList.tsx
@@ -1,11 +1,19 @@
+"use client";
 import Pagination from "@/components/modules/p-admin/Pagination";
 import Table from "@/components/modules/p-admin/Table";
 import ContactRow from "./ContactRow";
 import EmptyError from "@/components/modules/p-admin/EmptyError";
 import { conatctTableHeader } from "@/constants/data";
-import { IContactList } from "@/libs/types";
+import { IContactList, IContactUs } from "@/libs/types";
+import { useOptimistic } from "react";
 
 function ContactList({ data, pagination }: IContactList) {
+  const [optimisticContacts, deleteOptimistc] = useOptimistic(
+    data,
+    (allContacts, id) => {
+      return allContacts.filter((cat: IContactUs) => cat._id !== id);
+    },
+  );
   return (
     <div className="md:section-box">
       <div className="admin-table mt-5 overflow-hidden  rounded-md">
@@ -16,8 +24,13 @@ function ContactList({ data, pagination }: IContactList) {
             ))}
           </Table.Header>
           <Table.Body>
-            {data.map((conatct, index) => (
-              <ContactRow {...conatct} key={conatct._id} index={index + 1} />
+            {optimisticContacts.map((conatct, index) => (
+              <ContactRow
+                {...conatct}
+                key={conatct._id}
+                index={index + 1}
+                onDelete={deleteOptimistc}
+              />
             ))}
           </Table.Body>
         </Table>
