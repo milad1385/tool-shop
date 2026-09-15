878b5ff9b5f28a3feb2df947f721281839f3049e
Milad salamian <miladsalami1385@gmail.com>
2026-09-16 00:00:49 +0330

    fix proxy bug

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,54 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/auth";
 import { UserRoleEnums } from "./libs/types";
-import { verifyToken } from "./utils/helper";
 
 export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-  const accessToken = request.cookies.get("accessToken");
-
   const session = await auth();
 
   if (pathname.startsWith("/auth")) {
-    if (accessToken || session) {
+    if (session) {
       return NextResponse.redirect(new URL("/", request.url));
     }
     return NextResponse.next();
   }
 
   if (pathname.startsWith("/p-user")) {
-    if (!accessToken && !session) {
+    if (!session) {
       return NextResponse.redirect(new URL("/", request.url));
     }
     return NextResponse.next();
   }
 
   if (pathname.startsWith("/p-admin")) {
-    if (!accessToken && !session) {
+    if (!session) {
       return NextResponse.redirect(new URL("/", request.url));
     }
 
-    let isAdmin = false;
-
-    if (accessToken) {
-      try {
-        const user = verifyToken(accessToken.value);
-        isAdmin = user.roles.some((role: UserRoleEnums) =>
-          [UserRoleEnums.SUPER_ADMIN, UserRoleEnums.ADMIN].includes(role),
-        );
-      } catch (error) {
-        console.error("خطا در verifyToken:", error);
-      }
-    }
-
-    if (!isAdmin && session?.user) {
-      const roles = (session.user as any).roles || [];
-      isAdmin = roles.some((role: string) =>
-        [UserRoleEnums.SUPER_ADMIN, UserRoleEnums.ADMIN].includes(
-          role as UserRoleEnums,
-        ),
-      );
-    }
+    const roles = (session.user as any).roles || [];
+    const isAdmin = roles.some((role: string) =>
+      [UserRoleEnums.SUPER_ADMIN, UserRoleEnums.ADMIN].includes(
+        role as UserRoleEnums,
+      ),
+    );
 
     if (!isAdmin) {
       return NextResponse.redirect(new URL("/", request.url));
