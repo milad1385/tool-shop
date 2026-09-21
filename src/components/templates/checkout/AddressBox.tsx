b73d1b252b255b5af9ada7d139d5b8de1accb875
Milad salamian <miladsalami1385@gmail.com>
2026-09-21 23:19:44 +0330

    feat(AddressBox) : add active mode

--- src/components/templates/checkout/AddressBox.tsx
+++ src/components/templates/checkout/AddressBox.tsx
@@ -3,18 +3,26 @@ import { HiChevronLeft } from "react-icons/hi";
 import AddressModal from "./AddressModal";
 import { FaTruck } from "react-icons/fa";
 
-function AddressBox({ address }) {
+function AddressBox({ address, id, activeAddress, onActive }) {
+  const activeItem = activeAddress === id;
   return (
     <>
-      <div className="border border-yellow-500 p-4 md:cursor-pointer rounded-lg flex items-center gap-x-4 mt-5">
+      <div
+        onClick={() => onActive(id)}
+        className={`border ${activeItem ? "border-yellow-500" : "border-zinc-300"} p-4 md:cursor-pointer rounded-lg flex items-center gap-x-4 mt-5`}
+      >
         <FaTruck className="text-zinc-700 text-lg" />
         <div className="space-y-3 flex-1">
           <div className="flex items-center justify-between">
-            <h3 className="text-xs md:text-base text-yellow-500">
+            <h3
+              className={`text-xs md:text-base ${activeItem ? "text-yellow-500" : "text-zinc-400"}`}
+            >
               ارسال به آدرس انتخاب شده
             </h3>
             <Modal.Open name="address">
-              <span className="text-xs md:text-sm flex items-center gap-x-1 text-yellow-500">
+              <span
+                className={`text-xs md:text-sm flex items-center gap-x-1 ${activeItem ? "text-yellow-500" : "text-zinc-400"}`}
+              >
                 تغییر آدرس
                 <HiChevronLeft />
               </span>
