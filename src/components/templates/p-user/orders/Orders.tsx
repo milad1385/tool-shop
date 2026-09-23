e4598e4e03d29bb92dee7dc8585b5ce503915d3c
Milad salamian <miladsalami1385@gmail.com>
2026-09-23 23:25:16 +0330

    feat(Orders) : add empty state

--- src/components/templates/p-user/orders/Orders.tsx
+++ src/components/templates/p-user/orders/Orders.tsx
@@ -1,15 +1,18 @@
 import { IGetUserOrders, IUserOrders } from "@/libs/types";
 import { getUserOrders } from "@/services/orders.service";
 import OrderBox from "./OrderBox";
+import EmptyState from "@/components/modules/main/EmptyState";
 
 async function Orders({ status }: IGetUserOrders) {
   const orders = await getUserOrders({ status });
 
   return (
     <div className="flex flex-col gap-4">
-      {orders.map((order) => (
-        <OrderBox key={order._id} {...order} />
-      ))}
+      {orders.length ? (
+        orders.map((order) => <OrderBox key={order._id} {...order} />)
+      ) : (
+        <EmptyState description="موردی برای نمایش سفارش ها یافت نشد" title="سفارشی یافت نشد" />
+      )}
     </div>
   );
 }
