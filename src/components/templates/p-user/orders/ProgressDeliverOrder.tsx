b0f49bc29c4a6919cf98adc2f147a0af43cf69d9
Milad salamian <miladsalami1385@gmail.com>
2026-09-23 23:14:57 +0330

    feat(ProgressDeliverOrder) : add progress bar in order detail

--- src/components/templates/p-user/orders/ProgressDeliverOrder.tsx
+++ src/components/templates/p-user/orders/ProgressDeliverOrder.tsx
@@ -1,17 +1,22 @@
-import React from "react";
+import { IProgressDeliverOrder } from "@/libs/types";
+import { getOrderInfo, percentageClasses } from "@/utils/helper";
 
-function ProgressDeliverOrder() {
+function ProgressDeliverOrder({ status }: IProgressDeliverOrder) {
+  const { title, backgroundColor, color, percentage } = getOrderInfo(status);
+
   return (
     <div className="p-4">
-      <div className="flex items-center gap-x-1 text-green-600 font-DanaMedium mt-6">
+      <div
+        className={`flex items-center gap-x-1 ${color} font-DanaMedium mt-6`}
+      >
         <span>🚚</span>
-        <span className={`text-sky-500 font-Lalezar text-lg`}>
-          در حال ارسال
-        </span>
+        <span className={`${color} font-Lalezar text-lg`}>{title}</span>
       </div>
 
       <div className="w-full mt-4 flex flex-row-reverse bg-gray-100 rounded-full">
-        <div className={`bg-sky-500 py-1.5 rounded-full w-[50%]`}></div>
+        <div
+          className={`${backgroundColor} ${percentageClasses[percentage]} py-1.5 rounded-full`}
+        />
       </div>
     </div>
   );
