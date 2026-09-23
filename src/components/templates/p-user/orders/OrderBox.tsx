d4e71354ad60c49935c330a2967524c3e6602b12
Milad salamian <miladsalami1385@gmail.com>
2026-09-23 20:06:15 +0330

    feat(orderbox) : get and show order detail

--- src/components/templates/p-user/orders/OrderBox.tsx
+++ src/components/templates/p-user/orders/OrderBox.tsx
@@ -1,74 +1,69 @@
 import Button from "@/components/ui/Button";
-import { IOrderBox } from "@/libs/types";
+import { IUserOrders } from "@/libs/types";
+import { formatDate, formattedPrice, getOrderInfo } from "@/utils/helper";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
-function OrderBox({ className }: IOrderBox) {
+function OrderBox({
+  status,
+  createdAt,
+  trackingCode,
+  totalDiscount,
+  finalPrice,
+  items,
+  _id,
+}: IUserOrders) {
+  const { title, backgroundColor } = getOrderInfo(status);
   return (
-    <div className={`p-6 border rounded-2xl relative ${className}`}>
-      <div className="bg-green-500 rounded-tl-2xl font-Lalezar text-sm text-white py-3 px-6 rounded-br-2xl absolute top-0 left-0">
-        پرداخت موفق
+    <div className={`p-6 border rounded-2xl relative`}>
+      <div
+        className={`${backgroundColor} rounded-tl-2xl font-Lalezar text-sm text-white py-3 px-6 rounded-br-2xl absolute top-0 left-0`}
+      >
+        {title}
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 my-10 text-sm">
         <div>
           <span>تاریخ:</span>
-          <span className="mr-1 text-stone-500">3 خرداد 1402</span>
+          <span className="mr-1 text-stone-500">{formatDate(createdAt)}</span>
         </div>
         <div>
           <span>کد سفارش:</span>
-          <span className="mr-1 text-stone-500">#246585</span>
+          <span className="mr-1 text-stone-500">{trackingCode}#</span>
         </div>
         <div>
           <span>تخفیف:</span>
           <span className="mr-1 text-stone-500">
-            76.000 <span className="hidden md:inline-block">تومان</span>
+            {formattedPrice(totalDiscount)}{" "}
+            <span className="hidden md:inline-block">تومان</span>
           </span>
         </div>
         <div>
-          <span>جمع سبد خرید:</span>
+          <span>جمع سفارش:</span>
           <span className="mr-1 text-stone-500">
-            399.000 <span className="hidden md:inline-block">تومان</span>
+            {formattedPrice(finalPrice)}{" "}
+            <span className="hidden md:inline-block">تومان</span>
           </span>
         </div>
       </div>
       <div className="flex flex-col sm:flex-row justify-between items-center">
         <div className="flex mb-6">
-          <Link href="/products/1">
-            <Image
-              src="/images/product-1.jpg"
-              alt="image 1"
-              width={1920}
-              height={1080}
-              className="w-24"
-            />
-          </Link>
-          <Link href="/products/1">
-            <Image
-              src="/images/product-2.jpg"
-              alt="image 2"
-              width={1920}
-              height={1080}
-              className="w-24"
-            />
-          </Link>
-          <Link href="/products/1">
-            <Image
-              src="/images/product-3.jpg"
-              alt="image 3"
-              width={1920}
-              height={1080}
-              className="w-24"
-            />
-          </Link>
+          {items.map((item) => (
+            <Link href={`/products/${item.product.slug}`} key={item._id}>
+              <Image
+                src={item.product.images[0]}
+                alt={item.product.name}
+                width={1920}
+                height={1080}
+                className="w-24"
+              />
+            </Link>
+          ))}
         </div>
         <div className="flex items-center gap-x-4">
-          <Link href="/factor/1">
-            <Button className="!rounded-xl !w-[125px]">
-              مشاهده فاکتور
-            </Button>
+          <Link href={`/factor/${_id}`}>
+            <Button className="!rounded-xl !w-[125px]">مشاهده فاکتور</Button>
           </Link>
-          <Link href="/p-user/orders/1">
+          <Link href={`/p-user/orders/${_id}`}>
             <Button className="!bg-red-600 !rounded-xl !w-[125px]">
               جزییات
             </Button>
