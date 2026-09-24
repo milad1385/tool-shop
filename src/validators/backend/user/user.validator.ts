d8a90731f6f95729a9099231a68e6b4c3bc6360e
Milad salamian <miladsalami1385@gmail.com>
2026-09-24 23:32:08 +0330

    feat(user validator) : add update user validator

--- src/validators/backend/user/user.validator.ts
+++ src/validators/backend/user/user.validator.ts
@@ -20,5 +20,33 @@ export const loginSchema = z.object({
   password: z.string().min(8, "رمز عبور حداقل ۸ کاراکتر باید باشد"),
 });
 
+export const updateUserValidorSchema = z.object({
+  name: z
+    .string()
+    .min(1, "حداقل تعداد کاراکتر 1 عدد است")
+    .max(100, "حداکثر تعداد کاراکتر 100 عدد میباشد"),
+
+  username: z
+    .string()
+    .min(1, "حداقل تعداد کاراکتر 1 عدد است")
+    .max(100, "حداکثر تعداد کاراکتر 100 عدد میباشد"),
+
+  email: z
+    .string()
+    .min(1, "حداقل تعداد کاراکتر 1 عدد است")
+    .email("ایمیل معتبر نمیباشد")
+    .max(100, "حداکثر تعداد کاراکتر 100 عدد میباشد"),
+
+  phone: z
+    .string()
+    .min(11, "حداقل تعداد کاراکتر 11 عدد است")
+    .max(11, "حداکثر تعداد کاراکتر 11 عدد میباشد")
+    .regex(/^[0-9]+$/, "شماره تلفن باید فقط شامل اعداد باشد"),
+
+  image: z.any(),
+});
+
+export type UpdateUserType = z.infer<typeof updateUserValidorSchema>;
+
 export type RegisterSchemaType = z.infer<typeof registerSchema>;
 export type LoginSchemaType = z.infer<typeof loginSchema>;
