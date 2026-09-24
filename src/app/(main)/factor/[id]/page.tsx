2dbebe0b93557c0ad7e1a1e0722de1f58bad227c
Milad salamian <miladsalami1385@gmail.com>
2026-09-24 11:55:02 +0330

    fix(factor) : fix redirect to not found page

--- src/app/(main)/factor/[id]/page.tsx
+++ src/app/(main)/factor/[id]/page.tsx
@@ -1,10 +1,13 @@
-import React from "react";
 import FactorDetails from "@/components/templates/p-user/orders/FactorDetails";
 import { IPage } from "@/libs/types";
 import { getUserOrder } from "@/services/orders.service";
+import { notFound } from "next/navigation";
 async function Factor({ params }: IPage) {
   const { id } = await params;
   const factor = await getUserOrder({ id });
+  if (!factor) {
+    notFound();
+  }
   return <FactorDetails factor={factor} />;
 }
 
