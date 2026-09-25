62f11d47c458d1ab52f394aa23993a46b10ad396
Milad salamian <miladsalami1385@gmail.com>
2026-09-25 17:05:14 +0330

    feat(products) : get and show products by filters

--- src/components/templates/products/Products.tsx
+++ src/components/templates/products/Products.tsx
@@ -1,26 +1,40 @@
 import Pagination from "@/components/modules/main/Pagination";
-import ProductBox from "@/components/modules/main/ProductBox";
-import { products } from "@/constants/data";
-import React, { Suspense } from "react";
-import SortProduct from "./SortProduct";
-import MobileSearch from "./MobileSearch";
-import MobileFilter from "./MobileFilter";
 import PaginationFallback from "@/components/modules/main/PaginationFallback";
+import ProductBox from "@/components/modules/main/ProductBox";
+import { getProductsWithFilter } from "@/services/products.service";
+import { Suspense } from "react";
+import MobileFilter from "./MobileFilter";
+import MobileSearch from "./MobileSearch";
+import SortProduct from "./SortProduct";
+import EmptyState from "@/components/modules/main/EmptyState";
 
-function Products() {
+async function Products({ searchParams }) {
+  const { page, category } = await searchParams;
+
+  const { data, pagination } = await getProductsWithFilter({
+    page: +page,
+    categorySlugs: category,
+  });
   return (
     <div className="col-span-12 md:col-span-9">
       <MobileSearch />
       <MobileFilter />
       <SortProduct />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-        {products.map((product) => (
-          <ProductBox {...product} key={product.id} />
-        ))}
-      </div>
+      {data.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+          {data.map((product) => (
+            <ProductBox {...product} key={product._id} />
+          ))}
+        </div>
+      ) : (
+        <EmptyState
+          title="محصولی یافت نشد"
+          description="با این فیلتر محصولی یافت نشد"
+        />
+      )}
       <Suspense fallback={<PaginationFallback />}>
         <div className="w-full">
-          <Pagination count={4} />
+          <Pagination count={pagination.totalPages} />
         </div>
       </Suspense>
     </div>
